4bbf5f06a43b5ba50438e6eb7113dd9b0c4eea57
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-04 17:05:10 +0530

    Time: 2 ms (54.96%), Space: 7.4 MB (70.6%) - LeetHub

---
+++ 2608-count-the-digits-that-divide-a-number/2608-count-the-digits-that-divide-a-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int countDigits(int num) {
+        int originalNum=num;
+        int count=0;
+        while(num>0)
+        {
+            int lastDigit=num%10;
+            if(lastDigit!=0 && originalNum%lastDigit==0)
+                count++;
+            num=num/10;
+        }
+        return count++;
+    }
+};
