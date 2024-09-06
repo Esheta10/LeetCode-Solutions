d79dffcb280cfe76ae313df4903efb92acdfa1b0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-06 20:19:49 +0530

    Time: 0 ms (100%), Space: 10.3 MB (17.79%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -4,6 +4,7 @@ public:
         int n=nums.size();
         for(int i=n-1;i>=0;i--)
         {
+            int didSwap=0;
             for(int j=0;j<=i-1;j++)
             {
                 if(nums[j]>nums[j+1])
@@ -11,8 +12,11 @@ public:
                     int temp=nums[j+1];
                     nums[j+1]=nums[j];
                     nums[j]=temp;
+                    didSwap=1;
                 }
             }
+            if(didSwap==0)
+                break;
         }
     }
 };
