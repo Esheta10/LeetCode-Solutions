cc2003d29785336ffddc6191edcea81628fcdbe0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-09 13:07:30 +0530

    Time: 20 ms (18.94%), Space: 21.3 MB (17.97%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -9,13 +9,30 @@ public:
             sum -=nums[i];
         }
         return sum; */
-        /*APPROACH-2*/
+        /*APPROACH-2
         int n=nums.size();
         int result=n;
         for(int i=0;i<n;i++)
         {
             result ^= i;
             result ^= nums[i];
+        }
+        return result;  */
+        /*APPROACH-3*/
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        int l=0, r=n-1;
+        int result=n;
+        while(l<=r)
+        {
+            int mid = l+(r-l)/2;
+            if(nums[mid]>mid)
+            {
+                result=mid;
+                r=mid-1;
+            }
+            else
+                l=mid+1;
         }
         return result;
     }
