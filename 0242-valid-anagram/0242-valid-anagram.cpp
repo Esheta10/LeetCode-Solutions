a88bc9b22d9eb898a6e9792eac79aae9b957a541
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-11 21:19:43 +0530

    Time: 4 ms (89.48%), Space: 8.8 MB (55.2%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,25 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        /*approach-1
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
 
-        return s==t;
+        return s==t;*/
+        vector<int> count(26,0);
+        for(char ch:s)
+        {
+            count[ch-'a']++;
+        }
+        for(char ch:t)
+        {
+            count[ch-'a']--;
+        }
+        //check if all the elements in count are zero
+        bool allZeroes = all_of(count.begin(),count.end(),[](int element)
+        {
+            return element == 0;
+        });
+        return allZeroes;
     }
 };
