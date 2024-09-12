7e8085a659d3e914a7e8034b2c140060eebccc6a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-12 18:49:10 +0530

    Time: 9 ms (83.97%), Space: 14.8 MB (71.63%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.cpp
@@ -17,6 +17,7 @@ public:
                 return false;
         }
         return true;*/
+       /*APPROACH-2
         int n=words.size();
         int count[26]={0};
        for(string &word:words)
@@ -29,6 +30,18 @@ public:
             if(freq % n != 0)
                 return false;
         }
-        return true;
+        return true;*/
+        int n = words.size();
+        int count[26]={0};
+        for(string &word:words)
+        {
+            for(char &ch:word)
+                count[ch-'a']++;
+        }
+       auto lambda=[&](int freq)
+       {
+        return freq%n==0;
+       };
+        return all_of(begin(count),end(count),lambda);
     }
 };
