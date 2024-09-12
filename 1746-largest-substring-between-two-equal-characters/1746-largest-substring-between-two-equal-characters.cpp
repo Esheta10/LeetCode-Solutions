db8b02d995373d0b2799edbb1acee4c2fe5dc23b
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-12 21:48:54 +0530

    Time: 0 ms (100%), Space: 7.8 MB (57.08%) - LeetHub

--- 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
@@ -15,6 +15,7 @@ public:
             }
         }
         return result;  */
+       /*approach-2
         int n=s.length();
         int result=-1;
         unordered_map<char,int> mp;
@@ -28,6 +29,22 @@ public:
                 result=max(result,i-mp[ch]-1);
             }
         }
+        return result;*/
+        int n=s.length();
+        int result=-1;
+        vector<int> count(26,-1);
+        for(int i=0;i<n;i++)
+        {
+            char ch=s[i];
+            if(count[ch-'a']==-1)
+               count[ch-'a']=i;
+            else
+            {
+                result = max(result,i-count[ch-'a']-1);
+            }
+
+        }
         return result;
+
     }
 };
