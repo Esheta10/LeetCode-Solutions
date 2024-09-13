04b9eeadbb3d0b9be67bb34e0b8a3704cdf246a3
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-13 23:37:21 +0530

    Time: 0 ms (100%), Space: 7.3 MB (89.61%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -5,9 +5,14 @@ public:
             return false;
 
         return (n&(n-1))==0;    */
-        if(n<=0)
+       /*approach-2 if(n<=0)
             return false;
         int count=__builtin_popcount(n);
-        return count==1;
+        return count==1;*/
+        if(n==0)
+            return false;
+        if(n==1)
+            return true;
+        return n%2==0 && isPowerOfTwo(n/2);
     }
 };
