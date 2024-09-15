8a0194ca01c7b6a7e6cb88682f346b31d803a638
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-15 19:11:04 +0530

    Time: 0 ms (100%), Space: 7.4 MB (85.84%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        if(n==0)
+            return 0;
+        int a=0;
+        int b=1;
+        int c=1;
+        int temp=0;
+        for(int i=3;i<=n;i++)
+        {
+            temp = a+b+c;
+            a=b;
+            b=c;
+            c=temp;
+        }
+        return c;
+    }
+};
