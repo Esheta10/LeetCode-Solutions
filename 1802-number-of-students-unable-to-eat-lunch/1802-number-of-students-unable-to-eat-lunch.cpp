6b2dd87e6e33ed896dc29f99408be5ae2129b981
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-15 10:16:48 +0530

    Time: 5 ms (32.93%), Space: 10.9 MB (98.4%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        int n=students.size();
+      /*APPROACH-1  int n=students.size();
         stack<int> st;
         queue<int> q;
         int count=0;
+       // Initialize stack and queue
        for(int i=0;i<n;i++)
         {
-            st.push(sandwiches[n-i-1]);
-            q.push(students[i]);
+            st.push(sandwiches[n-i-1]);// Fill stack with sandwiches
+            q.push(students[i]);// Fill queue with students
         }
         while(!st.empty() && !q.empty())  
         {
@@ -16,7 +17,7 @@ public:
                 {
                     st.pop();
                     q.pop();
-                    count=0;
+                    count=0;// Reset count when a sandwich is eaten
                 }
                 else
                 {
@@ -25,12 +26,30 @@ public:
                     q.push(temp);//adds the student to the back of the queue.
                     count++;
 
-                    if(count==q.size())
+                    if(count==q.size())// No student can eat the current sandwich
                         break;
                 }
 
         }
     
-        return st.size();
+        return st.size();// Return the number of uneaten sandwiches*/
+        int n = students.size();
+        int arr[2]={0};
+        //arr[0]->count of students liking 0 sandwich
+        //arr[1]->count of students liking 1 sandwich
+        for(int &stud:students)
+        {
+            arr[stud]++;
+        }
+        for(int i=0;i<n;i++)
+        {
+            int sand=sandwiches[i];
+            if(arr[sand]==0)
+                return n-i;
+
+            arr[sand]--;
+
+        }
+        return 0;
     }
 };
