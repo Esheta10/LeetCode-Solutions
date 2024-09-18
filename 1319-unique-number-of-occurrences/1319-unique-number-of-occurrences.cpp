9900a989e43d610ced42b52dc2eec1c0c72ec8a0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-18 10:56:56 +0530

    Time: 0 ms (100%), Space: 12.3 MB (5.01%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
+    /*APPROACH-1
         unordered_map<int,int> mp;
-
+        //store frequency of each numbers
         for(int &num:arr)
             mp[num]++;
 
@@ -13,8 +14,20 @@ public:
             if(st.find(freq)!=st.end())
                 return false;
 
-                st.insert(freq);
+             st.insert(freq);
                
+        }
+        return true;    */
+        vector<int> vec(2001,0);
+        for(int &x:arr)
+            vec[x+1000]++;
+
+        sort(vec.begin(),vec.end());
+
+        for(int i=1;i<2001;i++)
+        {
+            if(vec[i]!=0 && vec[i]==vec[i-1])
+                return false;
         }
         return true;
     }
