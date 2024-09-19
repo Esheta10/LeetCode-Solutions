522fedc42232a381d59337c6392c3838f1d0b88d
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-19 20:00:55 +0530

    Time: 684 ms (17.05%), Space: 480.5 MB (15.31%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string findroot(string &word,unordered_set<string>& st)
+    {
+        for(int l=0;l<word.length();l++)
+        {
+            string root = word.substr(0,l);
+            if(st.contains(root))
+                return root;
+        }
+        return word;
+    }
+    string replaceWords(vector<string>& dictionary, string sentence) {
+        unordered_set<string> st(dictionary.begin(),dictionary.end());
+
+        stringstream ss(sentence);
+        string word;
+        string result;
+
+        while(getline(ss,word,' '))
+        {
+            result += findroot(word,st) + " ";
+        }
+        result.pop_back();
+        return result;
+    }
+};
