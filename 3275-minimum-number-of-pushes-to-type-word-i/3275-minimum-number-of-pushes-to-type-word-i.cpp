ddd4f42d1a02dd9b0ac5c020bb89e8629869f113
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-19 13:48:57 +0530

    Time: 5 ms (21.07%), Space: 8.4 MB (48.86%) - LeetHub

--- 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/3275-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,34 @@
 class Solution {
 public:
     int minimumPushes(string word) {
+        /*APPROACH-1
         //2,3,4,5,6,7,8,9
-        unordered_map<int,int> mp;
+        unordered_map<int,int> mp;//You can use an array of size 10 also and use index 2 to 9
         int result = 0;
         int assign_key = 2;
         for(char &ch:word)
         {
-            if(assign_key>9)
+            if(assign_key>9)// If the assign_key exceeds 9, reset it to 2 (keypad has keys from 2 to 9)
                 assign_key=2;
 
-            mp[assign_key]++;
-            result += mp[assign_key];
+            mp[assign_key]++; // Increment the push count for the current assign_key
+            result += mp[assign_key]; // Add the current key's push count to the total result
 
-            assign_key++;
+            assign_key++;// Move to the next key (like pressing the next key on a keypad)
+        }
+        return result; // Return the total number of pushes required    */
+        vector<int> mp(26,0);
+        for(char &ch:word)
+        {
+            mp[ch-'a']=1;//Mentioned in qn, all letters will be distinct
+        }
+        sort(mp.begin(),mp.end(),greater<int>());//descending order of freq
+        int result = 0;
+        for(int i=0;i<26;i++)
+        {
+            int freq = mp[i];
+            int press = i/8 +1;
+            result += press * freq;
         }
         return result;
     }
