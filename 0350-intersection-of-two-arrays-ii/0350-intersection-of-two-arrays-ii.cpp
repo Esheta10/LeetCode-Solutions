03a5c709a246a908bda734413a3bcb0a1f23e1ed
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-09-20 11:20:25 +0530

    Time: 0 ms (100%), Space: 13 MB (57.79%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+       /*APPROACH-1
+       //T.C:  O(n)
+       //S.C : O(n)
         unordered_map<int,int> mp;
         for(int &num : nums1)
         {
@@ -12,10 +15,32 @@ public:
             int num = nums2[i];
             if(mp[num]>0)
             {
-                mp[num]--;
+                mp[num]--;//dobara agar woh number fir se aata hai tb hum use again result mein nahi add karenge
                 result.push_back(num);
             }
+        }
+        return result;*/
+        /*APPROACH-2*/
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        int i=0;// -> nums1
+        int j=0;// -> nums2
+        vector<int> result;
+        while(i < nums1.size() && j < nums2.size())
+        {
+            if(nums1[i] == nums2[j])
+            {
+                result.push_back(nums1[i]);//result.push_back(nums2[j]);
+                i++;
+                j++;
+            } 
+            else if(nums1[i]<nums2[j])
+                i++;
+            else    
+                j++;  
         }
         return result;
     }
 };
+//T.C:  O(n)
+//S.C : O(n)
