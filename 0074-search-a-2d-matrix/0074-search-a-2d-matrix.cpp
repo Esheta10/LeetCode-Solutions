4efbda3be71e04113f9b25aa576ad958769a8676
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-11-24 14:28:02 +0530

    Time: 0 ms (100%), Space: 12.2 MB (39.65%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        //Approach-1 O(m*n)
+        /*
         int m = matrix.size();
         int n = matrix[0].size();
 
@@ -14,6 +16,28 @@ public:
             else
                 return true;
         }
+        return false;   */
+        //Approach-2 (Using Binary Search) O(log(m*n))
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int start = 0;
+        int end= m*n-1;
+
+        while(start<=end)
+        {
+            int mid = start+(end-start)/2;
+            int row=mid/n;
+            int col=mid%n;
+
+            if(matrix[row][col]<target)
+                start=mid+1;
+            else if(matrix[row][col]>target)
+                end=mid-1;
+            else
+                return true;
+        }
         return false;
+
     }
 };
