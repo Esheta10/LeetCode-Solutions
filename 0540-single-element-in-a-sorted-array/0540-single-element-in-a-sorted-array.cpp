b2c995949888d5cdeab52ff525ece648b66d9009
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-11-24 17:32:33 +0530

    Time: 0 ms (100%), Space: 24.9 MB (86.1%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,11 +1,34 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
+        //Approach-1:XOR Operation:-(O(n))
         int result=0; 
         for(int i=0;i<nums.size();i++)
         {
             result ^= nums[i];
         }
         return result;
+        //Approach-2:Binary Search:-O(log(n))
+        int l=0;
+        int h=nums.size()-1;
+        while(l<h)
+        {
+            int mid=l+(h-l)/2;
+            if(mid % 2 == 0)//mid is even
+            {
+                if(nums[mid]==nums[mid+1])
+                    l=mid+2;
+                    else
+                    h=mid;
+            }
+            else//mid is odd
+            {
+                if(nums[mid]==nums[mid-1])
+                    l=mid+1;
+                else
+                    h=mid;
+            }
+        }
+        return nums[l];
     }
 };
