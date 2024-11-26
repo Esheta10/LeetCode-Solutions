d02e5389f7b9b8b885536e1e9f9c13b856413d1f
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-11-26 15:25:14 +0530

    Time: 0 ms (100%), Space: 43.5 MB (13.4%) - LeetHub

--- 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
+++ 0954-maximum-sum-circular-subarray/0954-maximum-sum-circular-subarray.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    int KadanesMax(vector<int>& nums,int n)
+int kadaneMax(const vector<int>& arr)
     {
-        int sum=nums[0];
-        int maxSum=nums[0];
-        for(int i=1;i<n;i++)
-        {
-            sum=max(sum+nums[i],nums[i]);
-            maxSum=max(maxSum,sum);
-        }
-        return maxSum;
+    int maxSum = INT_MIN, currentSum = 0;
+    for (int num : arr) {
+        currentSum = max(num, currentSum + num);
+        maxSum = max(maxSum, currentSum);
     }
-    int KadanesMin(vector<int>& nums,int n)
+    return maxSum;
+    }
+    int kadaneMin(const vector<int>& arr)
     {
-        int sum=nums[0];
-        int minSum=nums[0];
-        for(int i=1;i<n;i++)
-        {
-            sum=min(sum+nums[i],nums[i]);
-            minSum=min(minSum,sum);
-        }
-        return minSum;
+    int minSum = INT_MAX, currentSum = 0;
+    for (int num : arr) {
+        currentSum = min(num, currentSum + num);
+        minSum = min(minSum, currentSum);
+    }
+    return minSum;
     }
     int maxSubarraySumCircular(vector<int>& nums) {
-        int n=nums.size();
-        //1.total sum
-        int SUM=accumulate(nums.begin(),nums.end(),0);
-        //2.minSum
-        int minSum=KadanesMin(nums,n);
-        //3.maxSum
-        int maxSum=KadanesMax(nums,n);//case-1
-        //4.circular_sum = SUM-minNSum
-        int circular_sum = SUM - minSum;
-        if(maxSum>0)
-            return max(maxSum,circular_sum);
-        
-        return maxSum;
+    int maxNormal = kadaneMax(nums);
+    if (maxNormal < 0) // All elements are negative
+        return maxNormal;
+
+    int totalSum = 0;
+    for (int num :nums)
+        totalSum += num;
+
+    int minSubarraySum = kadaneMin(nums);
+    int maxCircular = totalSum - minSubarraySum;
+
+    return max(maxNormal, maxCircular);
     }
 };
