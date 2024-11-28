bf13f7b57bd3305f567565fe602dc21bd2f8ef08
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-11-28 13:26:18 +0530

    Time: 32 ms (92.86%), Space: 104.4 MB (6.81%) - LeetHub

--- 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
+++ 2392-successful-pairs-of-spells-and-potions/2392-successful-pairs-of-spells-and-potions.cpp
@@ -1,7 +1,26 @@
 class Solution {
 public:
+    int BinarySearch(int l,int r,vector<int>& potions,int minPotion)
+    {
+        int index = -1;
+        int mid=0;
+
+        while(l<=r)
+        {
+            mid = l+(r-l)/2;
+            if(potions[mid]>=minPotion)
+            {
+                index=mid;
+                r=mid-1;
+            }
+            else
+                l=mid+1;
+        }
+        return index;
+    }
     vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
-        int m = spells.size();
+        //Approach-1 : Using lower_bound STL
+        /*int m = spells.size();
         int n = potions.size();
 
         sort(potions.begin(),potions.end());
@@ -20,6 +39,29 @@ public:
                 continue;
             }
             int index = lower_bound(begin(potions),end(potions),minPotion)-begin(potions);
+//It performs a binary search to find the first position in the sorted range [begin(potions), end(potions)) where the value is not less than minPotion.
+            answer.push_back(n-index);
+        }
+        return answer;*/
+        int m = spells.size();
+        int n = potions.size();
+
+        vector<int> answer;
+        sort(potions.begin(),potions.end());
+        int maxPotion = potions[n-1];
+
+        for(int i=0;i<m;i++)
+        {
+            int spell=spells[i];
+            long long minPotion = ceil((1.0*success)/spell);
+
+            if(minPotion > maxPotion)
+            {
+                answer.push_back(0);
+                continue;
+            }
+            int index = BinarySearch(0,n-1,potions,minPotion);
+            
             answer.push_back(n-index);
         }
         return answer;
