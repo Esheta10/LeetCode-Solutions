65883ceec240283839a253adfab180d4995e5ba9
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-12-01 13:00:24 +0530

    Time: 0 ms (100%), Space: 20.7 MB (19.16%) - LeetHub

--- 0136-single-number/0136-single-number.cpp
+++ 0136-single-number/0136-single-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int xorr=0;
+        int result = 0;
         for(int i=0;i<nums.size();i++)
         {
-            xorr ^= nums[i];
+            result = result^nums[i];
         }
-        return xorr;
+        return result;
     }
 };
