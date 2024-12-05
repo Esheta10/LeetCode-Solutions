d7bef71903125acf7bec8ec167f9e076ee8252dd
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-12-05 11:14:54 +0530

    Time: 3 ms (31.51%), Space: 11.2 MB (11.79%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
+//Approach-1 (Using shift)
+//T.C : O(log(n)), where n is the maximum number of bits needed to represent the given input integers left and right.
+//S.C : O(1)
     int rangeBitwiseAnd(int left, int right) {
-        int shiftCount=0;
+      /*  int shiftCount=0;
         while(left!=right)
         {
           left = left >> 1;
           right = right >> 1;
           shiftCount++;
         }
-        return left << shiftCount;
+        return left << shiftCount;*/
+        while(right>left)
+        {
+            right = right & (right-1);
+        }
+        return right;
     }
 };
