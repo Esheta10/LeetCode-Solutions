139b8ad58df7851a244f3c9deda76817baf30190
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-12-24 11:17:19 +0530

    Time: 71 ms (29.89%), Space: 114.9 MB (18.24%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void twoSum(vector<int>& nums,int target,int i,int j)
+    void twoSum(vector<int>nums,int target,int i,int j)
     {
         while(i<j)
         {
-            int sum=nums[i]+nums[j];
-            if(sum>target)j--;
-            else if(sum<target)i++;
+            if(nums[i]+nums[j]>target)
+                j--;
+            else if(nums[i]+nums[j]<target)
+                i++;
             else
             {
-                //sum==target-->first we'll remove duplicates from either end (i,j)
-                while(i<j && nums[i]==nums[i+1])i++;
-                while(i<j && nums[j]==nums[j-1])j--;
+                //first we will remove duplicates from either end
+                while(i<j && nums[i]==nums[i+1])
+                    i++;
+                while(i<j && nums[j]==nums[j-1])
+                    j--;
+
                 result.push_back({-target,nums[i],nums[j]});
                 i++;
                 j--;
-
             }
         }
     }
@@ -24,19 +27,16 @@ public:
         int n=nums.size();
         if(n<3)
             return {};
-        result.clear();
-        //sort
         sort(nums.begin(),nums.end());
-        //fixing one element n1
-        for(int i=0;i<=n-3;i++)
+        for(int i=0;i<n;i++)
         {
-            if(i>0 && nums[i]==nums[i-1])
-            {
+            if(i!=0 && nums[i]==nums[i-1])
                 continue;
-            }
-            int n1=nums[i];
-            int target=-n1;
-            twoSum(nums,target,i+1,n-1);//it will find n1,n2 & n3 : {n1,n2,n3}
+            
+            int n1 = nums[i];
+            int target = -n1;
+
+            twoSum(nums,target,i+1,n-1);
         }
         return result;
     }
