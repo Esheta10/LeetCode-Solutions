80d76d3212c5f1a0fb64d44fe60bd73b5e6408f9
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2024-12-28 22:00:09 +0530

    Time: 0 ms (100%), Space: 25.3 MB (48.11%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-    vector<int> findErrorNums(vector<int>& nums) {
-         int n = nums.size();
+     vector<int> findErrorNums(vector<int>& nums) {
+        int n = nums.size();
         
         int dup     = -1;
         int missing = -1;
