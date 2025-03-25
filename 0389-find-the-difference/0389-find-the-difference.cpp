f39abcc55aeecdbc25121ffb2d946bfe3159438d
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-03-25 19:20:04 +0530

    Time: 0 ms (100%), Space: 9.5 MB (17.24%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,36 +1,19 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-       /* APPROACH-1*/
-       int XOR=0;
-        for(char &ch:s)
-        {
-            XOR ^=ch;
-        }
-        for(char &ch:t)
-        {
-            XOR ^=ch;
-        }
-        return (char)XOR;
-        /*APPROACH-2---->o(n)->space
+        
         unordered_map<char,int> mp;
+
         for(char &ch:s)
+        {
             mp[ch]++;
+        }
         for(char &ch:t)
         {
             mp[ch]--;
             if(mp[ch]<0)
                 return ch;
         }
-        return 'x';//flow will never come here*/
-        /*APPROACH-3
-        int sum_s=0,sum_t=0;
-        for(char &ch:s)
-            sum_s +=ch;
-        for(char &ch:t)
-            sum_t +=ch;
-        //sum_t>sum_s
-        return (char)(sum_t-sum_s);*/
-
+        return 'x';
     }
 };
