6fab9c97b31bd7cf61b5f00f612798a84c53925c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-03-25 19:27:41 +0530

    Time: 0 ms (100%), Space: 9 MB (94.84%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.cpp
+++ 0389-find-the-difference/0389-find-the-difference.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         
+    /*  -----APPROACH-1-----
         unordered_map<char,int> mp;
 
         for(char &ch:s)
@@ -15,5 +16,26 @@ public:
                 return ch;
         }
         return 'x';
+        */
+        /* -----APPROACH-2-----(doesn't satisfy all the test cases)
+        unordered_map<char,int> mp;
+        for(char &ch:s)
+        {
+            mp[ch]++;
+        }
+        for(char &ch:t)
+        {
+            if(mp.find(ch)==mp.end())//the given character is not present in the string,then return it
+                return ch;
+        }
+        return 'x';
+    */
+    int sum_s=0,sum_t=0;
+    for(char &ch:s)
+        sum_s += ch;
+    for(char &ch:t)
+        sum_t += ch;
+
+    return (char)(sum_t-sum_s);
     }
 };
