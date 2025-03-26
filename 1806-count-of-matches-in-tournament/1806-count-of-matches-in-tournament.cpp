bb0b108618f22af2f476975cb478050b434c1d15
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-03-26 13:57:59 +0530

    Time: 0 ms (100%), Space: 7.8 MB (64.75%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int numberOfMatches(int n) {
-        return (n-1);
+    /* APPROACH-1
+       return (n-1);    */
+       int matches=0;
+       while(n>1)
+       {
+            if(n%2==0)//even
+            {
+                matches += n/2;
+                n=n/2;
+            }
+            if(n%2!=0)
+            {
+                matches += (n-1)/2;
+                n=(n-1)/2 +1;
+            }
+       }
+       return matches;
     }
 };
