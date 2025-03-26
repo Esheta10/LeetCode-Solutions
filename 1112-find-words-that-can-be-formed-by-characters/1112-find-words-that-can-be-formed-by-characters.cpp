e1923e35428ae4e59a11f5c75dcbd0f628a5c9fb
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-03-26 13:17:38 +0530

    Time: 5 ms (83.93%), Space: 24.1 MB (68.22%) - LeetHub

--- 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
     int countCharacters(vector<string>& words, string chars) {
-        // Count frequency of each character in chars
-        vector<int> charCount(26,0);
-        for(char &ch:chars)
-            charCount[ch-'a']++;
-
-        int result=0;
-        // Process each word
-        for(string &word:words)
-        {   
-            vector<int> wordCount(26,0);
-            // Count frequency of each character in word
-            for(char &ch:word)
-                wordCount[ch-'a']++;
         
-        bool ok=true;
-        for(int i=0;i<26;i++)
+        int result=0;
+        vector<int> charCount(26,0);
+        for(char &ch: chars)
         {
-            if(wordCount[i]>charCount[i])
-            {
-                 ok=false;
-                 break;
-            }
+            charCount[ch-'a']++;
         }
-        // If word can be formed, add its length to the result
-        if(ok==true)
-            result += word.length(); 
-    }
-       return result;
+      
+        for(string &word:words)
+        {
+            vector<int> wordCount(26,0);
+            for(char &ch:word)
+              wordCount[ch-'a']++;
+        
+            bool ok=true;
+            for(int i=0;i<26;i++)
+            {
+                if(wordCount[i]>charCount[i])
+                {
+                    ok=false;
+                    break;
+                }
+            }
+            if(ok==true)
+                result += word.size();
+        }
+        return result;
     }
 };
