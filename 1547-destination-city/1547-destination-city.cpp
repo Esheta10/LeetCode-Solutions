6a2dff376566651b91b5a5641523062d4fbf757f
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-03-27 11:25:53 +0530

    Time: 0 ms (100%), Space: 14.9 MB (79.94%) - LeetHub

--- 1547-destination-city/1547-destination-city.cpp
+++ 1547-destination-city/1547-destination-city.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
+        /*----APPROACH-1----
         unordered_map<string,int> mp;
         for(auto &path:paths)
         {
@@ -11,6 +12,20 @@ public:
         {
             string dest=path[1];
             if(mp[dest]!=1)
+                return dest;
+        }
+        return "";      */
+        
+        unordered_set<string> st;
+        for(auto &path:paths)
+        {
+            string source=path[0];//source
+            st.insert(source);
+        }
+        for(auto &path:paths)
+        {
+            string dest=path[1];
+            if(st.find(dest)==st.end())
                 return dest;
         }
         return "";
