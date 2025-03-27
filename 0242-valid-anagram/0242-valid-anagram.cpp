56a1657a9f19b625d513850e578ac63f8db46fbc
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-03-27 11:34:55 +0530

    Time: 7 ms (22.04%), Space: 9.7 MB (74.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,25 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        /*approach-1
         sort(s.begin(),s.end());
         sort(t.begin(),t.end());
-
-        return s==t;*/
-        vector<int> count(26,0);
-        for(char ch:s)
-        {
-            count[ch-'a']++;
-        }
-        for(char ch:t)
-        {
-            count[ch-'a']--;
-        }
-        //check if all the elements in count are zero
-        bool allZeroes = all_of(count.begin(),count.end(),[](int element)
-        {
-            return element == 0;
-        });
-        return allZeroes;
+        return s==t;
     }
 };
