488df40cc4c05c969655299702c3ce6ae5b61a64
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-16 23:31:56 +0530

    Time: 3 ms (51.36%), Space: 15.1 MB (51.9%) - LeetHub

--- 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.cpp
+++ 2025-redistribute-characters-to-make-all-strings-equal/2025-redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,47 +1,20 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
-       /*APPROACH-1
-        int n=words.size();
+        int n = words.size();
         unordered_map<char,int> mp;
         for(string &word:words)
         {
             for(char &ch:word)
+            {
                 mp[ch]++;
+            }
         }
         for(auto &it:mp)
         {
-            //char ch=it.first;
-            int freq=it.second;
-            if(freq % n != 0)
+            if(it.second % n != 0)
                 return false;
-        }
-        return true;*/
-       /*APPROACH-2
-        int n=words.size();
-        int count[26]={0};
-       for(string &word:words)
-       {
-        for(char &ch:word)
-            count[ch-'a']++;
-       }
-        for(auto &freq:count)
-        {
-            if(freq % n != 0)
-                return false;
-        }
-        return true;*/
-        int n = words.size();
-        int count[26]={0};
-        for(string &word:words)
-        {
-            for(char &ch:word)
-                count[ch-'a']++;
-        }
-       auto lambda=[&](int freq)
-       {
-        return freq%n==0;
-       };
-        return all_of(begin(count),end(count),lambda);
+        } 
+        return true;
     }
 };
