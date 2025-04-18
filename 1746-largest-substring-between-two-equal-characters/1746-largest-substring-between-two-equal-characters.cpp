575ee3ffa75d3eefc7e85db910e955cafbead92a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-18 23:13:06 +0530

    Time: 2 ms (13.9%), Space: 8.7 MB (78.85%) - LeetHub

--- 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/1746-largest-substring-between-two-equal-characters.cpp
@@ -1,50 +1,19 @@
 class Solution {
 public:
     int maxLengthBetweenEqualCharacters(string s) {
-      /*approach-1
-        int n=s.length();
-        int result=-1;
-        for(int i=0;i<=n-2;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                if(s[i]==s[j])
-                {
-                    result=max(result,j-i-1);
+       int result = -1;
+        int n      = s.length();
+        
+        for(int i = 0; i < n-1; i++) {
+            for(int j = i+1; j<n; j++) {
+                
+                if(s[i] == s[j]) {
+                    result = max(result, j-i-1);
                 }
+            
             }
         }
-        return result;  */
-       /*approach-2
-        int n=s.length();
-        int result=-1;
-        unordered_map<char,int> mp;
-        for(int i=0;i<n;i++)
-        {
-            char ch=s[i];
-            if(mp.find(ch)==mp.end())
-                mp[ch]=i;
-            else
-            {
-                result=max(result,i-mp[ch]-1);
-            }
-        }
-        return result;*/
-        int n=s.length();
-        int result=-1;
-        vector<int> count(26,-1);
-        for(int i=0;i<n;i++)
-        {
-            char ch=s[i];
-            if(count[ch-'a']==-1)
-               count[ch-'a']=i;
-            else
-            {
-                result = max(result,i-count[ch-'a']-1);
-            }
-
-        }
+        
         return result;
-
     }
 };
