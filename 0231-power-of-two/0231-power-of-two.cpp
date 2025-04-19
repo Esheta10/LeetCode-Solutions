6931f56927cae649089bfb5e39a8da870cd37d52
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-19 21:24:21 +0530

    Time: 0 ms (100%), Space: 7.9 MB (53.7%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,18 +1,9 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        /*approach-1if(n<=0)
-            return false;
-
-        return (n&(n-1))==0;    */
-       /*approach-2 if(n<=0)
-            return false;
-        int count=__builtin_popcount(n);
-        return count==1;*/
-        if(n==0)
-            return false;
-        if(n==1)
-            return true;
+        //Approach-1
+        if(n<=0)    return false;
+        if(n==1)    return true;
         return n%2==0 && isPowerOfTwo(n/2);
     }
 };
