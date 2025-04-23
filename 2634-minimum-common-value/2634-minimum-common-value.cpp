a7d67617292edc7106f4e2826ad3c05a51666970
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-23 18:05:45 +0530

    Time: 2 ms (42.59%), Space: 54.5 MB (59.42%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.cpp
+++ 2634-minimum-common-value/2634-minimum-common-value.cpp
@@ -14,13 +14,28 @@ public:
         }
         return -1;*/
         //APPROACH-2: Using unordered set
-        unordered_set<int> st(nums1.begin(),nums1.end());
+        /* unordered_set<int> st(nums1.begin(),nums1.end());
         for(int &num: nums2)
         {
             if(st.find(num)!=st.end())
                 return num;
         }
+        return -1; */
+        int m = nums1.size();
+        int n = nums2.size();
+
+        int i = 0;
+        int j = 0;
+
+        while(i<m && j<n)
+        {
+            if(nums1[i]==nums2[j])
+                return nums1[i];//nums2[j]
+            else if(nums1[i]<nums2[j])
+                i++;
+            else
+                j++;
+        }
         return -1;
-        
     }
 };
