08170665c8ef5bfc5132f2797d102c7d235f55c1
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-23 14:01:47 +0530

    Time: 45 ms (14.42%), Space: 69.7 MB (13.16%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.cpp
+++ 2634-minimum-common-value/2634-minimum-common-value.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        /* APPROACH-1: Using unordered map
         unordered_map<int,int> mp;
         for(int &num:nums1)
         {
@@ -11,6 +12,15 @@ public:
             if(mp.find(num)!=mp.end())
                 return num;
         }
+        return -1;*/
+        //APPROACH-2: Using unordered set
+        unordered_set<int> st(nums1.begin(),nums1.end());
+        for(int &num: nums2)
+        {
+            if(st.find(num)!=st.end())
+                return num;
+        }
         return -1;
+        
     }
 };
