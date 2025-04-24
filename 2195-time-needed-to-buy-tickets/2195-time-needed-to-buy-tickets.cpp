0855e876161ef1792323a0f30b5da45b9c020f34
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-24 12:57:59 +0530

    Time: 0 ms (100%), Space: 10.4 MB (83.45%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -2,13 +2,12 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int time=0;
-        int n=tickets.size();
-        for(int i=0;i<n;i++)
+        for(int i=0;i<tickets.size();i++)
         {
             if(i<=k)
-                time += min(tickets[i],tickets[k]);
+                 time += min(tickets[i],tickets[k]);
             else
-                time += min(tickets[k]-1,tickets[i]);
+                 time += min(tickets[k]-1,tickets[i]);
         }
         return time;
     }
