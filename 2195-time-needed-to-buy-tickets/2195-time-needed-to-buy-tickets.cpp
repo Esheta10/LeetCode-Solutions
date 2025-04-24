24b654cb3c84aacf86109952806ca7993774e6b8
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-04-24 13:41:21 +0530

    Time: 0 ms (100%), Space: 10.4 MB (83.45%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
+        /*APPROACH-1 :(Using Array for simulation)*/
+        //T.C : O(m*n) - Outer loop will run until nums[k] is 0. So in worst case nums[k] has max value m 
+        //S.C : O(1)
+        int n = tickets.size();
+        int time = 0;
+        
+        if(tickets[k]==1)
+            return k+1;
+
+        while(tickets[k]>0)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(tickets[i]!=0)
+                {
+                    tickets[i]--;
+                    time++;
+                }
+                if(tickets[k]==0)
+                return time;
+            }
+        }
+        return time;
+        /*APPROACH-2: Simple Observation
+        //T.C : O(n)
+        //S.C : O(1)
         int time=0;
         for(int i=0;i<tickets.size();i++)
         {
@@ -9,6 +35,6 @@ public:
             else
                  time += min(tickets[k]-1,tickets[i]);
         }
-        return time;
+        return time;*/
     }
 };
