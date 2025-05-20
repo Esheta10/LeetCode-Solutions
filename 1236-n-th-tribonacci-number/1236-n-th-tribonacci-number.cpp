3635af367c272508b67a10e13548ff36e3a5ad85
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-05-20 16:26:31 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    //APPROACH-1
+    /*
     int t[38];
     int find(const int &n)
     {
@@ -18,5 +20,24 @@ public:
     int tribonacci(int n) {
         memset(t,-1,sizeof(t));
         return find(n);
+    */
+    int tribonacci(int n) {
+    if(n==0)
+        return 0;
+    if(n==1 || n==2)
+        return 1;
+    int a=0;
+    int b=1;
+    int c=1;
+    int d=a+b+c;
+    for(int i=3;i<=n;i++)
+    {
+        d=a+b+c;
+
+        a=b;
+        b=c;
+        c=d;
+    }
+    return d;
     }
 };
