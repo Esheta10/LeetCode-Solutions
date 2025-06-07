6b98978212fef19a076c4952f366471e3621a597
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-07 13:48:29 +0530

    Time: 7 ms (46%), Space: 16.3 MB (48.89%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -3,28 +3,37 @@ public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int n = score.size();
         vector<string> result(n);
-        priority_queue<pair<int,int>> pq;//max-heap
+        unordered_map<int,int> mp;
 
         for(int i=0;i<n;i++)
         {
-            pq.push({score[i],i});
+            mp[score[i]]=i;
         }
-        int rank=1;
-        while(!pq.empty())
-        {
-            int idx=pq.top().second;
-            pq.pop();
-        
-                if(rank==1)
-                    result[idx]="Gold Medal";
-                else if(rank==2)
-                    result[idx]="Silver Medal";
-                else if(rank==3)
-                    result[idx]="Bronze Medal";
-                else
-                    result[idx]=to_string(rank);
 
-                rank++;
+        sort(score.begin(),score.end(),greater<int>());
+
+        for(int i=0;i<n;i++)
+        {
+            if(i==0)
+            {
+                int ath = mp[score[i]];
+                result[ath]="Gold Medal";
+            }
+            else if(i==1)
+            {
+                int ath = mp[score[i]];
+                result[ath]="Silver Medal";
+            }
+            else if(i==2)
+            {
+                int ath = mp[score[i]];
+                result[ath]="Bronze Medal";
+            }
+            else
+            {   
+                int ath=mp[score[i]];
+                result[ath]=to_string(i+1);
+            }
         }
         return result;
     }
