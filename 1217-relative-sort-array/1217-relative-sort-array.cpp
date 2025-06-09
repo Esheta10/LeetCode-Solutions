675ef48c31d9a42807a1478b5646656c0d7720b2
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-09 20:06:48 +0530

    Time: 2 ms (29.45%), Space: 11.3 MB (21.86%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.cpp
+++ 1217-relative-sort-array/1217-relative-sort-array.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        map<int,int> mp;
-
-        for(int i=0;i<arr2.size();i++)
+        map<int,int> mp;    //ordered_map;
+        for(int &num:arr1)
         {
-            mp[arr2[i]]=i;//2->0, 1->1, 4->2, 3->3, 9->4, 6->5
+            mp[num]++;
         }
-        for(int &num : arr1)
+        int i=0;
+        for(int &num:arr2)
         {
-            if(!mp.contains(num))
-                mp[num]=1e9;//arr1 ke index ki value infinity kar do
+            while(mp[num]-->0)
+                arr1[i++]=num;
         }
-
-        auto lambda = [&](int &num1,int &num2){
-            if(mp[num1] == mp[num2])//in case of infinity
-                return num1 < num2;//return the lesser of the two values like return 7 before 19
-                return mp[num1] < mp[num2];
-        };
-        sort(arr1.begin(),arr1.end(),lambda);
+        for(auto &it:mp)
+        {
+            int freq=it.second;
+            while(freq>0)
+            {
+                arr1[i++]=it.first;
+                freq--;
+            }
+        }
         return arr1;
     }
 };
