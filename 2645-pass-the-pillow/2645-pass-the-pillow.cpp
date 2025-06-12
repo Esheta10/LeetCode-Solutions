d718386234475dfc8145ff61790dc132b35ef226
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-12 18:54:54 +0530

    Time: 0 ms (100%), Space: 7.7 MB (80.68%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int passThePillow(int n, int time) {
+        //Approach-1(Simulations)
+        //T.C. = O(time)
+        //S.C. = O(1)
+        /*
         int idx = 1;
         int dir = 1;
         while(time > 0)
@@ -14,5 +18,16 @@ public:
                 dir *= -1;
         }
         return idx;
+        */
+        int fullRounds = time/(n-1);
+        int timeLeft = time%(n-1);
+
+        if(fullRounds % 2==0)
+            return timeLeft+1;//you are at first position
+        else
+            return n-timeLeft;
+
+
+        return -1;
     }
 };
