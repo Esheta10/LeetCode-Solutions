a06e39dafe3f2e7871a5c52964b5423f956e54c4
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-13 19:38:02 +0530

    Time: 0 ms (100%), Space: 87.2 MB (72.49%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.cpp
+++ 0890-lemonade-change/0890-lemonade-change.cpp
@@ -3,27 +3,29 @@ public:
     bool lemonadeChange(vector<int>& bills) {
         int five=0;
         int ten=0;
-        for(int &bill : bills)
+        for(int &bill:bills)
         {
-            if(bill==5)
+            if(bill == 5)
+            {
                 five++;
-            else if(bill==10)//return $5
+            }
+            else if(bill == 10)//return 5$ to customer
             {
                 if(five>0)
                 {
                     five--;
                     ten++;
                 }
-                else
+                else 
                     return false;
             }
-            else if(five > 0 && ten >0)//return $5,$5,$5 or $10,$5 for $20 bill
+            else if(five>0 && ten>0)//return 15$ to customer
             {
                 five--;
                 ten--;
             }
-            else if(five>=3)
-                five -= 3;//return $5,$5,$5
+            else if(five>=3)//5, 5, 5
+                five -= 3;
             else
                 return false;
         }
