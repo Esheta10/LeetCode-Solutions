76d4469fb8ca07a3f9cd8a44ed5082cb354c34a5
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-13 14:55:23 +0530

    Time: 0 ms (100%), Space: 14.1 MB (48.23%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
+        //Approach-1:
+        //T.C.=O(n)
+        //S.C.=O(1)
         int depth=0;
         for(string &log:logs)
         {
@@ -12,5 +15,22 @@ public:
                 depth++;
         }
         return depth;
+        //Approach-2 (Using Stack for simulation)
+        //T.C : O(n)
+        //S.C : O(n)
+        stack<string> st;
+        for(string &log:logs)
+        {
+            if(log=="../")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else if(log=="./")
+                continue;
+            else
+                st.push(log);
+        }
+        return st.size();
     }
 };
