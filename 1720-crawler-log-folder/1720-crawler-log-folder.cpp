b39d92ea69c3d0e3c13091ce2a53972789d94f1c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-13 13:14:18 +0530

    Time: 0 ms (100%), Space: 14 MB (67.1%) - LeetHub

--- 1720-crawler-log-folder/1720-crawler-log-folder.cpp
+++ 1720-crawler-log-folder/1720-crawler-log-folder.cpp
@@ -2,16 +2,12 @@ class Solution {
 public:
     int minOperations(vector<string>& logs) {
         int depth=0;
-        for(string &s : logs)
+        for(string &log:logs)
         {
-            if(s=="../")
-            {
-                depth = max(0,depth-1);
-            }
-            else if(s=="./")
-            {
+            if(log=="../")
+                depth=max(0,depth-1);
+            else if(log=="./")
                 continue;
-            }
             else
                 depth++;
         }
