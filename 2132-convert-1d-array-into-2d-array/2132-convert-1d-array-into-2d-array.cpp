d89eba358ecd776e1cb99ea55cce77aaf5aef086
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-16 15:55:11 +0530

    Time: 16 ms (35.75%), Space: 94.5 MB (27.79%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        //Approach-1 (Using simply simulation)
+        //T.C : O(m*n)
+        //S.C : O(1) //no including space of result
+        /*
         vector<vector<int>> result(m,vector<int>(n));
 
         if(original.size() != m*n)
@@ -14,6 +18,19 @@ public:
                 result[i][j] = original[idx];
                 idx++;
             }
+        }
+        return result;  */
+        vector<vector<int>> result(m,vector<int>(n));
+
+        if(original.size() != m*n)
+            return {};
+
+        for(int i=0;i<original.size();i++)
+        {
+            int row = i/n;
+            int col = i%n;
+
+            result[row][col] = original[i];
         }
         return result;
     }
