5c221855c2e173d817e2592fb71e23e3bc0c2ba4
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-16 14:54:32 +0530

    Time: 100 ms (5.21%), Space: 94.7 MB (20.62%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.cpp
@@ -1,37 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
-//Approach-1 (Using simply simulation)
-//T.C : O(m*n)
-//S.C : O(1) //no including space of result       
-    /*  vector<vector<int>> result(m,vector<int>(n));
-        int l=original.size();
+        vector<vector<int>> result(m,vector<int>(n));
 
-        if(l != m*n)
-        {
-            return {};
-        }
+        if(original.size() != m*n)
+            return {}; 
+
         int idx=0;
-        for(int row=0;row<m;row++)
+        for(int i=0;i<m;i++)
         {
-            for(int col=0;col<n;col++)
+            for(int j=0;j<n;j++)
             {
-                result[row][col]=original[idx];
+                result[i][j] = original[idx];
                 idx++;
             }
         }
-        return result; */
-         vector<vector<int>> result(m, vector<int>(n));
-         int l=original.size();
-         if(l!=m*n)
-         {
-            return {};
-         }
-        
-         for(int i=0;i<l;i++)
-         {
-            result[i/n][i%n]=original[i];
-         }
-         return result;
+        return result;
     }
 };
