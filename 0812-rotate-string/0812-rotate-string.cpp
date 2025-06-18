fb07929a7a436c8105224a24815a7bf53ba36ccd
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-18 16:48:02 +0530

    Time: 0 ms (100%), Space: 8.1 MB (61.56%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.cpp
+++ 0812-rotate-string/0812-rotate-string.cpp
@@ -4,14 +4,14 @@ public:
         int m = s.length();
         int n = goal.length();
 
-        if (m != n) 
+        if(m!=n)
             return false;
 
-        // Try all possible rotations of the string
-        for (int rotationCount = 1; rotationCount <= m; ++rotationCount) {
-            // Perform one rotation
-            rotate(s.begin(), s.begin() + 1, s.end()); //it says that I want (s.begin()+1)th character to become the first character now. i.e. shifting left by one
-            if (s == goal) 
+        for(int count = 0;count<m;count++)
+        {
+            rotate(s.begin(),s.begin()+1,s.end());
+
+            if(s == goal)
                 return true;
         }
         return false;
