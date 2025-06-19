c31d2eb9736215f83ce8c25366307c26bf4b048c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-19 19:35:46 +0530

    Time: 3 ms (28.7%), Space: 27.8 MB (21.4%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
+    #define P pair<int,int>
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        //Approach-1 (Brute Force)
+        //T.C : O(n * k)
+        //S.C : O(1)
+        /*
         int n = nums.size();
 
         for(int i=0;i<k;i++)
@@ -12,6 +17,27 @@ public:
                     idx = j;
             }
            nums[idx] *= multiplier;
+        }
+        return nums;    */
+
+        int n = nums.size();
+
+        priority_queue<P,vector<P>,greater<P>> pq;
+        for(int i=0;i<n;i++)
+        {
+            pq.push({nums[i],i});
+        }
+        while(k--)
+        {
+            pair<int,int> temp = pq.top();
+            pq.pop();
+
+            int number = temp.first;
+            int idx = temp.second;
+
+            nums[idx] = number*multiplier;
+            pq.push({nums[idx],idx});
+
         }
         return nums;
     }
