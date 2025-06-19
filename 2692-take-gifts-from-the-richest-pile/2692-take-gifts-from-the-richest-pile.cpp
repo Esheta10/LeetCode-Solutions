de2ade1d88e5b70e501a74e4fbf98e61dd54c3fc
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-19 15:51:39 +0530

    Time: 0 ms (100%), Space: 13.3 MB (20.75%) - LeetHub

--- 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
+++ 2692-take-gifts-from-the-richest-pile/2692-take-gifts-from-the-richest-pile.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     long long pickGifts(vector<int>& gifts, int k) {
-         //Heapify - O(n)
-        priority_queue<int> pq(begin(gifts), end(gifts)); //max-heap
-
+        // Heapify - O(n)
+        priority_queue<int> pq(gifts.begin(),gifts.end());  //max-heap
         long long sum = 0;
-        for(int &gift : gifts) { //O(n)
+        for(int &gift : gifts)  //O(n)
             sum += gift;
+
+        int maxEle = 0;
+        while(k--)  //O(k)
+        {
+            maxEle = pq.top();  //O(1)
+            pq.pop();//O(log(n))
+
+            int remaining = sqrt(maxEle);
+            sum -= (maxEle - remaining);
+            pq.push(remaining);
         }
-
-        while(k--) { //O(k)
-            int maxEl = pq.top(); //O(1)
-            pq.pop(); //O(log(n))
-
-            int remaining = sqrt(maxEl); //Ignoring this time complexity
-            sum -= (maxEl - remaining);
-            pq.push(remaining);//O(log(n))
-        }
-
         return sum;
     }
 };
