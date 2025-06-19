e5a80ac7d7a78a0624c51f8c458bb0a4cf5e40c4
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-19 13:47:58 +0530

    Time: 4 ms (15.36%), Space: 13.9 MB (29.7%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool checkIfExist(vector<int>& arr) {
+        // Approach-1: Brute Force
+        //T.C = O(n)
+        //S.C = O(n)
+        /*
         int n = arr.size();
 
         for(int i=0;i<n;i++)
@@ -10,6 +14,21 @@ public:
                 if(i!=j && arr[i] == 2*arr[j])
                     return true;
             }
+        }
+        return false;*/
+
+        int n = arr.size();
+
+        unordered_set<int> st;
+
+        for(int i=0;i<n;i++)
+        {
+            if(st.find(arr[i]*2) != st.end())
+                return true;
+            else if(arr[i]%2==0 && st.find(arr[i]/2) != st.end())
+                return true;
+
+            st.insert(arr[i]);
         }
         return false;
     }
