338568ef5b9ca03e3de14edd4afca557ceecccb8
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-19 18:26:49 +0530

    Time: 0 ms (100%), Space: 27.1 MB (86.29%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.cpp
@@ -3,18 +3,16 @@ public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         int n = nums.size();
 
-        for (int j = 0; j < k; j++) {
-
-            int idx = 0;
-            for (int i = 0; i < n; i++) {
-                if (nums[i] < nums[idx]) {
-                    idx = i;
-                }
+        for(int i=0;i<k;i++)
+        {
+            int idx=0;
+            for(int j=0;j<n;j++)
+            {
+                if(nums[j] < nums[idx])
+                    idx = j;
             }
-
-            nums[idx] *= multiplier;
+           nums[idx] *= multiplier;
         }
-
         return nums;
     }
 };
