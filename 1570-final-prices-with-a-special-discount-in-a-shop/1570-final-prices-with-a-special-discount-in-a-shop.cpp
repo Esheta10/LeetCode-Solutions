56f84eaa16e0dda10fb81101424bf4c67b402a03
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-20 15:47:40 +0530

    Time: 0 ms (100%), Space: 14.1 MB (49.55%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
+        //Approach-1 (Brute Force)
+        //T.C : O(n^2)
+        //S.C : O(1)
+        /*
         int n = prices.size();
 
         //vector<int> result(prices.begin(),prices.end());
@@ -15,6 +19,22 @@ public:
                 }
             }
         }
-        return prices;
+        return prices;*/
+        
+        int n = prices.size();
+        vector<int> result(prices.begin(),prices.end());
+
+        stack<int> st;
+
+        for(int i=0;i<n;i++)
+        {
+            while(!st.empty() && prices[i] <= prices[st.top()])
+            {
+                result[st.top()] -= prices[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return result;
     }
 };
