cf5977ad3273fe0080e50b23f2a87c566d55d7fb
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-06-21 20:49:02 +0530

    Time: 0 ms (100%), Space: 27.7 MB (58.04%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.cpp
+++ 3429-special-array-i/3429-special-array-i.cpp
@@ -3,14 +3,11 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         
         int n = nums.size();
-
-        for(int i = 0; i <= n-2; i++) {
-            if(nums[i]%2 == nums[i+1]%2) {
-                return false;
-            }
-        }
-
-        return true;
+       for(int i=0;i<=n-2;i++)
+       {
+         if(nums[i]%2 == nums[i+1]%2)
+            return false;
+       }
+       return true;
     }
 };
-    
