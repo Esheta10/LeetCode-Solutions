04a4b9f4f26aeb7e0b1de4e13eb8619be1d61841
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-08-25 19:05:04 +0530

    Time: 0 ms (100%), Space: 9.3 MB (49.46%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,67 +1,51 @@
 class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        if(matrix.size()==0)
-            return {};
-
+        vector<int> result;
         int m = matrix.size();
         int n = matrix[0].size();
+        int top = 0;
+        int down = m-1;
+        int left = 0;
+        int right = n-1;
 
-        vector<int> result;
-        int top=0;
-        int down=m-1;
-        int left=0;
-        int right=n-1;
-
-        int id=0;
-        //id
-        //0 ->left to right
-        //1 ->top to down
-        //2 ->right to left
-        //3 ->down to top
+        int dir=0;
 
         while(top<=down && left<=right)
         {
-            //left to right
-            if(id==0)
+            if(dir==0)
             {
                 for(int i=left;i<=right;i++)
-                {
                     result.push_back(matrix[top][i]);
-                }
+
                 top++;
             }
-            //top to down
-            if(id==1)
+            if(dir==1)
             {
                 for(int i=top;i<=down;i++)
-                {
                     result.push_back(matrix[i][right]);
-                }
+
                 right--;
             }
-            //right to left
-            if(id==2)
+            if(dir==2)
             {
                 for(int i=right;i>=left;i--)
-                {
                     result.push_back(matrix[down][i]);
-                }
+
                 down--;
             }
-            //bottom to top
-            if(id==3)
+            if(dir==3)
             {
                 for(int i=down;i>=top;i--)
-                {
                     result.push_back(matrix[i][left]);
-                }
+                
                 left++;
             }
-            id++;
-            if(id==4)
-                id=0;
 
+            dir++;
+
+            if(dir==4)
+                dir=0;
         }
         return result;
     }
