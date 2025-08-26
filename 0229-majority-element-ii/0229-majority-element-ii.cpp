aeffac6616b3625d3c89a2d6561f81e78ab551fa
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-08-26 17:25:21 +0530

    Time: 3 ms (54.62%), Space: 23.6 MB (61.81%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         int n = nums.size();
-
-        int count1 = 0;
-        int maj1 = NULL;
-
-        int count2 = 0;
-        int maj2 = NULL;
+        int count1=0;
+        int maj1=NULL;
+        int count2=0;
+        int maj2=NULL;
 
         for(int i=0;i<n;i++)
         {
-
             if(nums[i]==maj1)
                 count1++;
             else if(nums[i]==maj2)
@@ -31,24 +28,22 @@ public:
                 count1--;
                 count2--;
             }
-            
         }
         vector<int> result;
-        count1 = 0;
-        count2 = 0;
-        for(int num:nums)
+        int freq1=0;
+        int freq2=0;
+        for(int num: nums)
         {
-            if(num == maj1)
-                count1++;
-            else if(num == maj2)
-                count2++;
+            if(num==maj1)
+                freq1++;
+            else if(num==maj2)
+                freq2++;
         }
-        if(count1>floor(n/3))
+        if(freq1 > floor(n/3))
             result.push_back(maj1);
-        if(count2>floor(n/3))
+        if(freq2> floor(n/3))
             result.push_back(maj2);
 
         return result;
-
     }
 };
