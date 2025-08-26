e55cb4ae65befffeedebd49ad018983f827b92bd
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-08-27 02:12:20 +0530

    Time: 501 ms (27.14%), Space: 41.6 MB (40.47%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
+    int n;
+    int t[2501][2501];
+    int solve(vector<int>& nums,int i,int p)
+    {
+        if(i>=n)
+            return 0;
+        if(p!=-1 && t[i][p] !=-1)
+                return t[i][p];
+        //take-skip
+        //take
+        int take=0;
+        if(p==-1 || nums[p]<nums[i])
+            take = 1 + solve(nums,i+1,i);
+        //skip
+        int skip = solve(nums,i+1,p);
+        
+        if(p!=-1)
+            t[i][p] = max(take,skip);
+        return max(take,skip);
+    }
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> tails;
-        for(int num:nums)
-        {
-            auto it=lower_bound(tails.begin(),tails.end(),num);
-            if(it==tails.end())
-                tails.push_back(num);
-            else
-                *it=num;
-        }
-        return tails.size();
+        n = nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(nums,0,-1);
     }
 };
