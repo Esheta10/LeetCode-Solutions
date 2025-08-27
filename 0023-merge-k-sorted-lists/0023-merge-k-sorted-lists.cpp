eecb64749f32b71aee42fe149b679c7ed55058f5
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-08-27 08:16:01 +0530

    Time: 3 ms (64.26%), Space: 18.3 MB (81.78%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,45 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoSortedLists(ListNode *l1,ListNode *l2)
+    ListNode* mergeTwoSortedLists(ListNode* lists1, ListNode* lists2)
     {
-        if(l1==NULL)
-            return l2;
-        if(l2==NULL)
-            return l1;
-        if(l1->val <= l2->val)
+        if(lists1==NULL)
+            return lists2;
+        if(lists2==NULL)
+            return lists1;
+
+        if(lists1->val < lists2->val)
         {
-            l1->next=mergeTwoSortedLists(l1->next,l2);
-            return l1;
+            lists1->next = mergeTwoSortedLists(lists1->next,lists2);
+            return lists1;
         }
         else
         {
-            l2->next=mergeTwoSortedLists(l1,l2->next);
-            return l2;
+            lists2->next = mergeTwoSortedLists(lists2->next,lists1);
+            return lists2;
         }
         return NULL;
     }
-    ListNode* partitionAndMerge(int start,int end,vector<ListNode*>& lists)
+    ListNode* partitionMerge(int start,int end,vector<ListNode*>& lists)
     {
+        if(start >  end)
+            return NULL;
         if(start==end)
             return lists[start];
-        if(start>end)
-            return NULL;
-        
-        int mid = start + (end-start)/2;
 
-        ListNode *l1=partitionAndMerge(start,mid,lists);
-        ListNode* l2=partitionAndMerge(mid+1,end,lists);
+        int mid = start + (end-start)/2;
+        ListNode* l1 = partitionMerge(start,mid,lists);
+        ListNode* l2 = partitionMerge(mid+1,end,lists);
 
         return mergeTwoSortedLists(l1,l2);
     }
-    ListNode* mergeKLists(vector<ListNode*>& lists) 
-    {
-        int n=lists.size();
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        int k = lists.size();
 
-        if(n==0)
+        if(k==0)
             return NULL;
 
-        return partitionAndMerge(0,n-1,lists);
+        return partitionMerge(0,k-1,lists);
     }
 };
