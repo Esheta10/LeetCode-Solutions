bab2f13b6a5e281315613a579bf0e40075c22f99
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-08-29 14:09:27 +0530

    Time: 66 ms (54.62%), Space: 9 MB (92.63%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -6,15 +6,15 @@ public:
         if(n==0)
             return 0;
 
-        int minCount = INT_MAX;
         if(t[n] != -1)
             return t[n];
-        for(int i=1; i*i <= n ;i++)
+        int min_count = INT_MAX;
+        for(int i=1;i*i<=n;i++)
         {
-            int result = 1 + helper(n - i*i);
-            minCount = min(result,minCount);
+            int result = 1 + helper(n-i*i);
+            min_count = min(min_count,result);
         }
-        return t[n] = minCount;
+        return t[n]=min_count;
     }
     int numSquares(int n) {
         memset(t,-1,sizeof(t));
