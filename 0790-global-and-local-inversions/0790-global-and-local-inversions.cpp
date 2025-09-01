c206bbd35d7fbf166db1e7628c70a8afa4394674
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-09-01 08:03:59 +0530

    Time: 0 ms (100%), Space: 87 MB (91.06%) - LeetHub

--- 0790-global-and-local-inversions/0790-global-and-local-inversions.cpp
+++ 0790-global-and-local-inversions/0790-global-and-local-inversions.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     bool isIdealPermutation(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); ++i) {
-            // Check if the current element is displaced by more than one position
-            if (abs(nums[i] - i) > 1) {
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            if(abs(nums[i]-i) > 1)
                 return false;
-            }
         }
         return true;
     }
