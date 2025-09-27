22c6e07d70775282e58f207839e009f4f382e53c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-09-27 10:51:20 +0530

    Time: 176 ms (17.06%), Space: 16.8 MB (8.84%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.cpp
+++ 0611-valid-triangle-number/0611-valid-triangle-number.cpp
@@ -1,28 +1,41 @@
 class Solution {
 public:
-    int triangleNumber(vector<int>& nums) {
-         int n = nums.size();
-    int count = 0;
-
-    // Sort the array to make checking easier
-    sort(nums.begin(), nums.end());
-
-    // Iterate through each element as the largest side
-    for (int k = 2; k < n; ++k) {
-        int i = 0, j = k - 1;
-
-        // Use two pointers to find pairs of sides
-        while (i < j) {
-            // If nums[i] + nums[j] > nums[k], we can form a triangle
-            if (nums[i] + nums[j] > nums[k]) {
-                count += (j - i); // All pairs between i and j form valid triangles with nums[k]
-                j--; // Move j to the left to check for other pairs
+         int binarySearch(const vector<int>& nums, int l, int r, int target) {
+        int k = -1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] < target) {
+                k = mid;         // mid is valid
+                l = mid + 1;     // try to go right
             } else {
-                i++; // Move i to the right if the sum is too small
+                r = mid - 1;     // go left
             }
         }
+        return k;
     }
 
-    return count;
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        if (n < 3) return 0;
+
+        sort(nums.begin(), nums.end());
+        int count = 0;
+
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == 0) 
+                continue;  
+            
+            for (int j = i + 1; j < n; ++j) {
+
+                int sum = nums[i] + nums[j]; //sum of two sides
+                int k = binarySearch(nums, j + 1, n - 1, sum);
+                if (k != -1) 
+                    count += (k - j);
+            }
+        }
+
+        return count;
     }
 };
+
+
