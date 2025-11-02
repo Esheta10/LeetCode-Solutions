9c1a64eba319c0b21ac1e2116c3f4d1d1cb7c088
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-11-02 20:02:21 +0530

    Time: 271 ms (24.04%), Space: 192.6 MB (12.57%) - LeetHub

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -1,42 +1,38 @@
-//Approach - Using Cumulative Sum of Frequencies and trying all possible targets in nums
+//Approach - Using Difference Array Technique
 //T.C : O(maxVal), maxVal = maximum element in nums + k
 //S.C : O(maxVal), maxVal = maximum element in nums + k
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k, int numOperations) {
-        int maxEl = *max_element(begin(nums), end(nums)) + k;
+        int maxVal = *max_element(begin(nums), end(nums)) + k;
 
-        vector<int> freq(maxEl + 1, 0);
+        vector<int> diff(maxVal+2, 0);
+        unordered_map<int, int> freq;
 
-        for(int &num : nums) {
-            freq[num]++;
+        for(int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+
+            int l = max(nums[i]-k, 0);
+            int r = min(nums[i]+k, maxVal);
+
+            diff[l]++;
+            diff[r+1]--;
         }
 
-        //cumulative sum of frequenceis
-        for(int i = 1; i <= maxEl; i++) {
-            freq[i] += freq[i-1];
-        }
 
-        int result = 0;
-        for(int target = 0; target <= maxEl; target++) {
-            if(freq[target] == 0)
-                continue;
-            
-            int leftNum  = max(0, target-k); //l
-            int rightNum = min(maxEl, target+k); //r
+        int result = 1;
 
-            int totalCount  = freq[rightNum] - (leftNum > 0 ? freq[leftNum-1] : 0);
+        for(int target = 0; target <= maxVal; target++) {
+            diff[target] += (target > 0 ? diff[target-1] : 0);
 
-            int targetCount = freq[target] - (target > 0 ? freq[target-1] : 0);
+            int targetFreq     = freq[target];
+            int needConversion = diff[target] - targetFreq;
 
-            int needConversion = totalCount - targetCount;
+            int maxPossibleFreq = min(needConversion, numOperations);
 
-            int maxPossibleFreq = targetCount + min(needConversion, numOperations);
-
-            result = max(result, maxPossibleFreq);
+            result = max(result, targetFreq + maxPossibleFreq);
         }
 
         return result;
     }
 };
-
