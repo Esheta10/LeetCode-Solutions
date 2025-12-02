3590ca0fc543119ac51e24771b39b918bf98cd4e
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-02 23:09:24 +0530

    Time: 38 ms (77.89%), Space: 45.3 MB (69.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,17 +1,38 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // Brute - Force Approach
+        // int n = nums.size();
+        // int count = 0;
+        // for(int s=0;s<=n-1;s++){
+        //     for(int e=s; e<=n-1;e++){
+        //         int sum = 0;
+        //         for(int i=s;i<=e;i++){
+        //             sum += nums[i];
+        //         }
+        //         if(sum == k)
+        //             count++;
+        //     }
+        // }
+
+        // return count;
+
+        // Optimal Approach
+
+        int n = nums.size();
+
         unordered_map<int,int> mp;
-        int cumSum = 0;
-        int result = 0;
         mp.insert({0,1});
 
-        for(int i=0;i<nums.size();i++)
-        {
+        int result = 0;
+        int cumSum = 0;
+
+        for(int i=0;i<n;i++){
+            
             cumSum += nums[i];
-            if(mp.find(cumSum-k)!=mp.end()){
-                result += mp[cumSum-k];
-            }
+            if(mp.find(cumSum-k) != mp.end())
+                result +=  mp[cumSum-k];
+
             mp[cumSum]++;
         }
         return result;
