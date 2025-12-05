6f1bb009c7866886c8f72fbe3cab1191d20365e7
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-06 00:14:26 +0530

    Time: 0 ms (100%), Space: 11.4 MB (78.44%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -2,19 +2,36 @@ class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
         
+        // Approach - 1
+        // int n = digits.size();
+
+        // int carry = 1;
+
+        // for(int i=n-1;i>=0;i--){
+        //     int sum = digits[i] + carry;
+
+        //     digits[i] = sum % 10;
+        //     carry = sum / 10;
+        // }
+        // if(carry)
+        //     digits.insert(digits.begin(),1);
+
+        // return digits;
+
+        // Approach-2 
+
         int n = digits.size();
 
-        int carry = 1;
-
         for(int i=n-1;i>=0;i--){
-            int sum = digits[i] + carry;
 
-            digits[i] = sum % 10;
-            carry = sum / 10;
+            if(digits[i] < 9){
+            // agar digit 9 se choti ho toh directly 1 add karke return kar do
+                digits[i]++;
+                return digits;
+            }
+            digits[i]=0;
         }
-        if(carry)
-            digits.insert(digits.begin(),1);
-
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
