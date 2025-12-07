2db877b6ab09189732c5b409775f4ffa1b0f0d0d
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-07 12:36:26 +0530

    Time: 0 ms (100%), Space: 15.7 MB (32.03%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int gola_index =-1;
-        int n = nums.size();
-        for(int i=n-1;i>0;i--)
-        {
-            if(nums[i-1] < nums[i])
-            {
+        
+        int n  = nums.size();
+        int gola_index = -1;
+
+        for(int i=n-1; i>0; i--){
+
+            if(nums[i-1] < nums[i]){
                 gola_index = i-1;
                 break;
             }
         }
 
-        if(gola_index != -1)
-        {
-            int swap_index = gola_index;
-            for(int j=n-1; j>=gola_index+1; j--)
-            {
-                if(nums[j] > nums[gola_index])
-                {
-                    swap_index = j;
+        if(gola_index != -1){
+
+            int swap_index = -1;
+            for(int i=n-1; i>gola_index; i--){
+                if(nums[i] > nums[gola_index]){
+                    swap_index = i;
                     break;
                 }
             }
-            swap(nums[gola_index],nums[swap_index]);
+            swap(nums[gola_index], nums[swap_index]);
         }
-        reverse(nums.begin()+gola_index+1,nums.end());
+        reverse(nums.begin()+gola_index+1, nums.end());
     }
 };
