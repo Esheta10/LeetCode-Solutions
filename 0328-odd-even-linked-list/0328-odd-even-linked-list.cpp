73ed5f40774f7132f5b3c1fc25573a5a9741ab9a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-12 12:51:11 +0530

    Time: 0 ms (100%), Space: 15.6 MB (62.71%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,33 +11,25 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-         if(head == NULL || head->next == NULL)
+        
+        if(head==NULL || head->next==NULL)
             return head;
-        
-        
-        /*
-            Story : 
-            1. Hume odd and even ko segregate karna hai
-            2. To let's take two pointers to point to odd and even nodes
-            3. Unko alag alag segregate kardenge
-            4. Last me connect kardenge odd ko even se
-            
-        */
-        ListNode *odd = head;
-        ListNode *even = head->next;
-        
+
+        ListNode* odd = head;
+        ListNode* even = head->next;
+
         ListNode* evenStart = head->next;
 
-        while(even!=NULL && even->next!=NULL)
-        {
-            odd->next=even->next;
-            even->next=even->next->next;
+        while(even!=NULL && even->next!=NULL){
 
-            odd=odd->next;
-            even=even->next;
+            odd->next = even->next;
+            even->next = even->next->next;
 
+            odd = odd->next;
+            even = even->next;
         }
-        odd->next=evenStart;
+        odd->next = evenStart;
+
         return head;
     }
 };
