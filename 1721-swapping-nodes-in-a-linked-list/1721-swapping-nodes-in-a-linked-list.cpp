b1b17a48e6dec64587d4d40be7c52e5e90afbee7
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-14 15:46:05 +0530

    Time: 0 ms (100%), Space: 185.4 MB (9.12%) - LeetHub

--- 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
+++ 1721-swapping-nodes-in-a-linked-list/1721-swapping-nodes-in-a-linked-list.cpp
@@ -10,35 +10,61 @@
  */
 class Solution {
 public:
-    int findLength(ListNode* head){
+    // Approach-1 : Two - Pass
+    // int findLength(ListNode* head){
 
-        int l = 0;
-        while(head != NULL){
-            head = head->next;
-            l++;
-        }
-        return l;
-    }
-    ListNode* swapNodes(ListNode* head, int k) {
+    //     int l = 0;
+    //     while(head != NULL){
+    //         head = head->next;
+    //         l++;
+    //     }
+    //     return l;
+    // }
+    // ListNode* swapNodes(ListNode* head, int k) {
         
-        int L = findLength(head);
+    //     int L = findLength(head);
 
-        ListNode* Node1 = head;
-        int k_1 = k;
+    //     ListNode* Node1 = head;
+    //     int k_1 = k;
 
-        while(k_1 > 1){
-            Node1 = Node1->next;
-            k_1--;
-        }
+    //     while(k_1 > 1){
+    //         Node1 = Node1->next;
+    //         k_1--;
+    //     }
 
-        ListNode* Node2 = head;
-        int k_2 = L-k+1;
+    //     ListNode* Node2 = head;
+    //     int k_2 = L-k+1;
 
-        while(k_2 > 1){
-            Node2 = Node2->next;
-            k_2--;
-        }
-        swap(Node1->val, Node2->val);
+    //     while(k_2 > 1){
+    //         Node2 = Node2->next;
+    //         k_2--;
+    //     }
+    //     swap(Node1->val, Node2->val);
+    //     return head;
+    // }
+    
+    // Approach-1: One Pass
+    ListNode* swapNodes(ListNode* head, int k) {
+
+        ListNode* temp = head;
+
+        ListNode* p1 = NULL;
+        ListNode* p2 = NULL;
+
+        while(temp){
+            
+            if(p2 != NULL){
+                p2 = p2->next;
+            }
+
+            k--;
+            if(k == 0){
+                p1 = temp;
+                p2 = head;
+            }
+            temp = temp->next;
+        }        
+        swap(p1->val,p2->val);
         return head;
     }
 };
