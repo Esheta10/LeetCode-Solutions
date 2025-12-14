b678b6eb57027df1c92c098a5f4962e167da07aa
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-14 22:00:02 +0530

    Time: 5 ms (40.13%), Space: 124.5 MB (50.92%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -13,7 +13,7 @@ public:
     int pairSum(ListNode* head) {
         
         //Approach-1 (Using vector/array)
-        // vector<int> vec;
+        vector<int> vec;
 
         // while(head){
         //     vec.push_back(head->val);
@@ -29,24 +29,60 @@ public:
         // return result;
 
         //Approach-2 (Using stack)
-        stack<int> st;
+        // stack<int> st;
+        // ListNode* curr = head;
+
+        // while(curr){
+        //     st.push(curr->val);
+        //     curr = curr->next;
+        // }
+        // curr = head;
+        // int count = 1;
+        // int N = st.size();
+        // int result = 0;
+
+        // while(count <= N/2){
+        //     result = max(result, st.top() + curr->val);
+        //     curr = curr->next;
+        //     st.pop();
+        //     count++;
+        // }
+        // return result;
+
+        //Approach-3(Reversing the 2nd half of Linked List)
+
+        //3.a Find the middle node of Linked List
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast!=NULL && fast->next!=NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* mid = slow;
+
+        //3.b Reversing Part
+        ListNode* prev = NULL;
+        ListNode* nextNode = NULL;
+        while(mid){
+
+            nextNode = mid->next;
+            mid->next = prev;
+            prev = mid;
+            mid = nextNode;
+
+        }
+
+        //3.c Reversing Part
         ListNode* curr = head;
-
-        while(curr){
-            st.push(curr->val);
-            curr = curr->next;
-        }
-        curr = head;
-        int count = 1;
-        int N = st.size();
         int result = 0;
-
-        while(count <= N/2){
-            result = max(result, st.top() + curr->val);
+        while(prev){
+            result = max(result, prev->val + curr->val);
+            prev = prev->next;
             curr = curr->next;
-            st.pop();
-            count++;
         }
+
         return result;
     }
 };
