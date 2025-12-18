7cd0cb63870d6def4ad4558b4b6a7cef76d653ad
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2025-12-18 10:57:19 +0530

    Time: 0 ms (100%), Space: 23.6 MB (23.16%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -10,41 +10,40 @@
  */
 class Solution {
 public:
-    ListNode* reverseLL(ListNode* head)
-    {
+    ListNode* reverseLL(ListNode* head){
         if(head==NULL || head->next==NULL)
             return head;
-        
-        ListNode* last = reverseLL(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return last;
+
+            ListNode* last = reverseLL(head->next);
+            head->next->next = head;
+            head->next = NULL;
+            return last;
     }
-    void reorderList(ListNode* head) 
-    {
+    void reorderList(ListNode* head) {
+        
         if(head==NULL || head->next==NULL)
             return;
 
         ListNode* slow = head;
         ListNode* fast = head;
+
         while(fast!=NULL && fast->next!=NULL)
         {
             slow = slow->next;
             fast = fast->next->next;
         }
-        ListNode* rev= reverseLL(slow);
+        ListNode* revHead = reverseLL(slow);
+
         ListNode* curr = head;
-
-        while(rev->next!=NULL)
-        {
+        while(revHead->next){
+            
+            ListNode* tempRev = revHead->next;
             ListNode* temp = curr->next;
-            curr->next = rev;
-
-            ListNode* temp_rev = rev->next;
-            rev->next = temp;
-
+            curr->next = revHead;
+            revHead->next = temp;
             curr = temp;
-            rev = temp_rev;
+            revHead = tempRev;
+
         }
     }
 };
