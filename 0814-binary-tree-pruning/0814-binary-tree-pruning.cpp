5d7a72fc311d7fc8950791187ec6a13102a6295e
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-08 19:12:14 +0530

    Time: 0 ms (100%), Space: 12.3 MB (99.9%) - LeetHub

--- 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.cpp
@@ -11,31 +11,34 @@
  */
 class Solution {
 public:
-    bool isOnePresent(TreeNode* root){
+    // bool isOnePresent(TreeNode* root){
 
-        if(root == NULL)
-            return false;
+    //     if(root == NULL)
+    //         return false;
 
-        if(root->val == 1)
-            return true;
+    //     if(root->val == 1)
+    //         return true;
 
-        return isOnePresent(root->left) || isOnePresent(root->right);
-    }
+    //     return isOnePresent(root->left) || isOnePresent(root->right);   // kisi ek bhi subtree mein agar 1 mil gaya toh thik hai
+    // }
     TreeNode* pruneTree(TreeNode* root) {
         
         if(root == NULL)
             return NULL;
 
-        pruneTree(root->left);
-        pruneTree(root->right);
+        // Post-order traversal
+        root->left = pruneTree(root->left);
+        root->right = pruneTree(root->right);
 
-        if(!isOnePresent(root->left))
-            root->left = NULL;
+        // if(!isOnePresent(root->left)) // agar root ke left mein 1 nahi mila toh usko null set kardo
+        //     root->left = NULL;
 
-        if(!isOnePresent(root->right))
-            root->right = NULL;
+        // if(!isOnePresent(root->right))// agar root ke right mein 1 nahi mila toh usko null set kardo
+        //     root->right = NULL;
 
-        if(!isOnePresent(root->left) && !isOnePresent(root->right) && root->val == 0)
+        // if(!isOnePresent(root->left) && !isOnePresent(root->right) && root->val == 0)// agar root ke left aur right dono mein null hai and root khud bhi 0 hai then null set kardo
+
+        if(root->left == NULL && root->right == NULL && root->val == 0)
             return NULL;
 
         return root;
