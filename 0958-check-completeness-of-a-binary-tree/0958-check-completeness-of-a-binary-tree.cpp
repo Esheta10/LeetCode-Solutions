44b672fb3cc83123012711246f13616d9cbbe6ad
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-08 11:50:16 +0530

    Time: 0 ms (100%), Space: 13.6 MB (79.92%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -11,29 +11,54 @@
  */
 class Solution {
 public:
+    int countNodes(TreeNode* root){
+
+        if(root == NULL)
+            return 0;
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+    bool dfs(TreeNode* root, int idx, int totalNodes){
+
+        if(root == NULL)
+            return true;
+
+        if(idx > totalNodes)
+            return false;
+
+        return dfs(root->left, 2*idx,totalNodes) && dfs(root->right, 2*idx+1,totalNodes);
+    }
     bool isCompleteTree(TreeNode* root) {
         
         // Using Level-order Traversal (BFS)
-        queue<TreeNode*> q;
-        q.push(root);
+        // queue<TreeNode*> q;
+        // q.push(root);
 
-        bool past = false; // kya aapne pehle kiss NULL node ko dekha hai?
-        while(q.empty() == false){
+        // bool past = false; // kya aapne pehle kiss NULL node ko dekha hai?
+        // while(q.empty() == false){
 
-            TreeNode* node = q.front();
-            q.pop();
+        //     TreeNode* node = q.front();
+        //     q.pop();
 
-            if(node == NULL){
-                past = true;
-            } else {
-                if(past == true)
-                    return false;
+        //     if(node == NULL){
+        //         past = true;
+        //     } else {
+        //         if(past == true)
+        //             return false;
 
-                q.push(node->left);
-                q.push(node->right);
-            }
+        //         q.push(node->left);
+        //         q.push(node->right);
+        //     }
            
-        }
-        return true;
+        // }
+        // return true;
+
+        // Using DFS
+
+        int totalNodes = countNodes(root);
+
+        int i = 1;
+
+        return dfs(root, i, totalNodes);
     }
 };
