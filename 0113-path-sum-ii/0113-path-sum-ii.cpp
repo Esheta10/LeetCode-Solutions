16ae4a2fd64b764fcc20834260e8dd8b8be5c37d
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-08 19:35:18 +0530

    Time: 15 ms (18.63%), Space: 38.8 MB (20.22%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -11,24 +11,32 @@
  */
 class Solution {
 public:
-    void collectPaths(TreeNode* root, int curr, vector<int>& temp, vector<vector<int>>& result) {
-        if(!root)
+    vector<vector<int>> result;
+    void fill(TreeNode* root, int sum,vector<int> temp, int& targetSum){
+
+        if(root == NULL)
             return;
+
+        sum += root->val;
         temp.push_back(root->val);
-        if(root->left == NULL && root->right == NULL && root->val == curr) {
-            result.push_back(temp);
+
+        if(root->left == NULL && root->right == NULL){
+            if(sum == targetSum){
+                result.push_back(temp);
+            }
+            return;
         }
+        fill(root->left,sum,temp,targetSum);
+        fill(root->right,sum,temp,targetSum);
         
-        collectPaths(root->left, curr-root->val, temp, result);
-        collectPaths(root->right, curr-root->val, temp, result);
-        temp.pop_back();
     }
-    
-    vector<vector<int>> pathSum(TreeNode* root, int sum) {
-        vector<vector<int>> result;
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         
+        int sum = 0;
         vector<int> temp;
-        collectPaths(root, sum, temp, result);
+        fill(root,sum,temp,targetSum);
+
         return result;
+
     }
 };
