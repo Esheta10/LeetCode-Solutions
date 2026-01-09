9e8e0093a39f6137c53fb5ab559a50c5035edf9f
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-09 19:43:25 +0530

    Time: 2 ms (16.35%), Space: 29.1 MB (10.24%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -9,41 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-//Approach - Recursively finding the best path
-//T.C : O(n)
-//S.C : O(n)
 class Solution {
 public:
-    
     int maxSum;
-    
-    int solve(TreeNode* root) {
-        if(root == NULL)
+    int totalSum(TreeNode* root){
+
+        if(root==NULL)
             return 0;
+
+        int l = totalSum(root->left);
+        int r = totalSum(root->right);
+
+        int neeche_hi_mil_gaya_answer = l + r + root->val;
+        int dono_mein_se_koi_ek_acha = max(l,r) + root->val; // 2
+        int only_root_acha = root->val; // 3
+
+
+        maxSum = max({maxSum,neeche_hi_mil_gaya_answer, dono_mein_se_koi_ek_acha,only_root_acha});
         
-        int l = solve(root->left);
-        int r = solve(root->right);
-        
-        int neeche_hi_milgaya_answer = l + r + root->val; //(1)
-        
-        int koi_ek_acha = max(l, r) + root->val; //(2)
-        
-        int only_root_acha = root->val; //(3)
-        
-        maxSum = max({maxSum, neeche_hi_milgaya_answer, koi_ek_acha, only_root_acha});
-        
-        
-        //most important part
-        return max(koi_ek_acha, only_root_acha);
-        
+        // most important part ->  return keval 2 and 3 karenge
+        return max(dono_mein_se_koi_ek_acha,only_root_acha);
     }
-    
     int maxPathSum(TreeNode* root) {
+        
         maxSum = INT_MIN;
-        
-        solve(root);
-        
+
+        totalSum(root);
+
         return maxSum;
-        
     }
 };
