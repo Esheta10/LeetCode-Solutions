5c82bcd94f829569c2d95862f1b5601353cb8dfa
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-12 18:43:22 +0530

    Time: 0 ms (100%), Space: 10.5 MB (99.14%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -11,22 +11,56 @@
  */
 class Solution {
 public:
-    void inOrder(TreeNode* root, vector<int>& result){
+    // void inOrder(TreeNode* root, vector<int>& result){
+
+    //     if(root == NULL)
+    //         return;
+
+    //     inOrder(root->left,result);
+    //     result.push_back(root->val);
+    //     inOrder(root->right,result);
+    // }
+    vector<int> inorderTraversal(TreeNode* root) {
+
+        // Approach-1
+        // vector<int> result;
+
+        // inOrder(root,result);
+
+        // return result;
+
+        // Approach-2: Morris Inorder Traversal
+        //T.C : O(n)
+        //S.C : O(1)
 
         if(root == NULL)
-            return;
-
-        inOrder(root->left,result);
-        result.push_back(root->val);
-        inOrder(root->right,result);
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
+            return {};
 
         vector<int> result;
 
-        inOrder(root,result);
+        TreeNode* curr = root;
 
+        while(curr != NULL){
+
+            if(curr->left == NULL){
+                result.push_back(curr->val);
+                curr = curr->right;
+            } else {
+                TreeNode* leftChild = curr->left;
+
+                while(leftChild->right != NULL){
+
+                    leftChild = leftChild->right;
+                }
+
+                leftChild->right = curr;
+                // curr->left ko app NULL set kar sakte ho
+                TreeNode* temp = curr;
+                curr = curr->left;
+                temp->left = NULL;
+            
+            }
+        }
         return result;
-        
     }
 };
