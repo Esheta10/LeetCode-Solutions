7bbb23899f0bf9cf7acb59092cb57441094d0ee2
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-15 14:16:50 +0530

    Time: 4 ms (65.65%), Space: 20 MB (35.73%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,55 +1,44 @@
 class Solution {
 public:
-    bool topologicalSort(unordered_map<int,vector<int>>& adj, int n, vector<int>& indegree){
+    bool topologicalSort(unordered_map<int,vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion){
 
-        queue<int> que;
-        int count = 0;
-        for(int i=0; i<n;i++){
-            if(indegree[i]==0){
-                que.push(i);
-                count++;
-            }
+        visited[u] = true;
+        inRecursion[u] = true;
+
+        for(int &v: adj[u]){
+
+            if(!visited[v] && topologicalSort(adj,v,visited,inRecursion))
+                return true;
+
+            if(inRecursion[v]== true)
+                return true;
         }
+        inRecursion[u] = false;
+        return false;
 
-        while(!que.empty()){
-
-            int u = que.front();
-            que.pop();
-
-            for(int &v : adj[u]){
-
-                indegree[v]--;
-
-                if(indegree[v]==0){
-                    que.push(v);
-                    count++;
-                }
-            }
-        }
-        if(count == n)  // I was able to visit all nodes and complete the course
-            return true; // no cycle
-
-        return false; // means there was a cycle and I couldn't complete all courses
     }
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         
-        unordered_map<int,vector<int>> adj;
+        vector<bool> visited(numCourses, false);
+        vector<bool> inRecursion(numCourses, false);
 
-        vector<int> indegree(numCourses,0);    // visited check(Kahn's Algo)
+        unordered_map<int, vector<int>> adj;
 
+        for(auto &vec: prerequisites){
 
-        for(auto& vec: prerequisites){
             int a = vec[0];
             int b = vec[1];
 
-            // b--->a
+            // b--->a 
             adj[b].push_back(a);
-
-            // arrow --> jaa raha hai 'a' mein
-            indegree[a]++;
         }
 
-        // if cycle present, not possible
-        return topologicalSort(adj, numCourses, indegree);
+        for(int i=0;i<numCourses;i++){
+
+            if(!visited[i] && topologicalSort(adj,i,visited,inRecursion))
+                return false; //cycle hai
+        }
+
+        return true;
     }
 };
