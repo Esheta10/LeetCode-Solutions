8eb345906682bc5fee846963c255f5f20e689d00
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-17 20:17:20 +0530

    Time: 0 ms (100%), Space: 7.9 MB (20.61%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
-
-        if(n<=1)
-            return n;
-
-        if(dp[n] != -1)
-            return dp[n];
-
-        return dp[n] = solve(n-1, dp) + solve(n-2, dp);
-    }
     int fib(int n) {
         
-        // if(n==0)
-        //     return 0;
-        // if(n==1)
-        //     return 1;
+      if(n<=1)
+        return n;
 
-        // return fib(n-1) + fib(n-2);
+        int dp[n+1];
 
-        if(n <= 1)
-            return n;
+        dp[0] = 0;
+        dp[1] = 1;
 
-        vector<int> dp(n+1,-1);
+        for(int i=2; i<=n;i++){
 
-        return solve(n, dp);
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
