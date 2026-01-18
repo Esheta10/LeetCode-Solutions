4a035aa14b4d684540000eb5bbf528c387632b5e
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-18 11:35:08 +0530

    Time: 184 ms (51.19%), Space: 120.2 MB (55.18%) - LeetHub

---
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    typedef long long ll;
+    int n;
+    ll t[1000001][2];
+    ll solve(int idx, vector<int>& nums, bool isEven){
+
+        if(idx >= n)
+            return 0;
+
+        if(t[idx][isEven] != -1)
+            return t[idx][isEven];
+
+        ll skip = solve(idx+1, nums, isEven);
+
+        ll val = nums[idx];
+
+        if(isEven == false){
+            val = -val;
+        }
+
+        ll take = solve(idx+1, nums, !isEven) + val;
+
+        return t[idx][isEven] = max(skip,take);
+
+    }
+    long long maxAlternatingSum(vector<int>& nums) {
+        
+        n = nums.size();
+        memset(t,-1,sizeof(t));
+        return solve(0, nums, true);
+    }
+};
