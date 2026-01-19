f540724655079133b8c6dd751a96bcab455fc0e6
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-19 22:46:37 +0530

    Time: 0 ms (100%), Space: 11.4 MB (91.81%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    int m, n;
+    int m,n;
     int t[101][101];
-    
-    int solve(vector<vector<int>>& obstacleGrid, int i, int j) {
-        
-        if(i < 0 || i >= m || j < 0 || j >= n || obstacleGrid[i][j] != 0) {
-            return 0;
-        }
-        
-        if(t[i][j] != -1)
-            return t[i][j];
-        
+    int solve(vector<vector<int>>& obstacleGrid, int i, int j){
+
         if(i == m-1 && j == n-1)
             return 1;
-        
-        //Why we are not making [i][j] visited ?
-        //Because robot can only move down or right so it will never visit any visited cell again
-        //int temp = obstacleGrid[i][j];
-        //obstacleGrid[i][j] = -1;
-        
+
+        if(i < 0 || i >= m || j < 0 || j >= n || obstacleGrid[i][j] != 0)
+            return 0;
+
+        if(t[i][j] != -1)
+            return t[i][j];
+
         int right = solve(obstacleGrid, i, j+1);
-        int down  = solve(obstacleGrid, i+1, j);
-        
-        //obstacleGrid[i][j] = temp;
-        
+        int down = solve(obstacleGrid, i+1, j);
+
         return t[i][j] = right + down;
-        
+
     }
-    
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        
         m = obstacleGrid.size();
         n = obstacleGrid[0].size();
-        
+
         memset(t, -1, sizeof(t));
-        
+
+        if(obstacleGrid[0][0] == 1 || obstacleGrid[m-1][n-1] == 1)
+            return 0;
+
         return solve(obstacleGrid, 0, 0);
+
     }
 };
