75e04eb60f5520f21613c83820afcb23e6c53876
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-24 12:58:50 +0530

    Time: 13 ms (90.42%), Space: 18.8 MB (11.65%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
+    typedef long long ll;
     vector<vector<int>> result;
-    void twoSum(vector<int>& nums, long long  newTarget, int i, int j, int first,int second){
+    void twoSum(vector<int>& nums, ll remaining, int i, int j, int n1, int n2){
+
         while(i<j){
-            if( nums[i] + nums[j] <  newTarget)
+            ll sum = (ll)nums[i] + (ll)nums[j];
+            if(sum < remaining)
                 i++;
-            else if( nums[i] + nums[j] > newTarget)
+            else if(sum > remaining)
                 j--;
             else{
                 while(i<j && nums[i]==nums[i+1]) i++;
                 while(i<j && nums[j]==nums[j-1]) j--;
-
-                result.push_back({first,second,nums[i],nums[j]});
+                result.push_back({n1,n2, nums[i], nums[j]});
                 i++;
                 j--;
             }
@@ -20,28 +22,30 @@ public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         
         int n = nums.size();
-        if(n<4)
+        if(n < 4)
             return {};
         
         result.clear();
-        sort(nums.begin(),nums.end());
 
-        for(int i=0;i<n;i++){
+        //1. sort the elements
+        sort(nums.begin(), nums.end());
 
-            if(i>0 && nums[i]==nums[i-1])   
+        //2. fix two elements - n1 & n2
+        for(int i=0; i<n; i++){
+
+            if(i>0 && nums[i]==nums[i-1])// skip duplicates
                 continue;
 
-            for(int j=i+1;j<n;j++){
+            for(int j=i+1; j<n; j++){
 
-                if(j>i+1 && nums[j]==nums[j-1])
+                if(j>i+1 && nums[j]==nums[j-1]) // skip duplicates
                     continue;
 
-                long long  first = nums[i];
-                long long  second = nums[j];
-
-                long long newTarget = (long long)target - first - second;
-
-                twoSum(nums,newTarget,j+1,n-1,first,second);
+                // n1+n2+n3+n4 = target
+                // n3+n4 = target - n1 - n2;
+                // remaining = target - n1 - n2;
+                ll remaining = (ll)target - (ll)nums[i] - (ll)nums[j];
+                twoSum(nums, remaining, j+1, n-1, nums[i], nums[j]);
             }
         }
         return result;
