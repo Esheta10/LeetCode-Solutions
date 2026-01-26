2d03535e4f988d4f4b610bdae8ceaa9dc81da827
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-26 21:15:04 +0530

    Time: 39 ms (73.31%), Space: 45.3 MB (69.5%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,40 +1,24 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        // Brute - Force Approach
-        // int n = nums.size();
-        // int count = 0;
-        // for(int s=0;s<=n-1;s++){
-        //     for(int e=s; e<=n-1;e++){
-        //         int sum = 0;
-        //         for(int i=s;i<=e;i++){
-        //             sum += nums[i];
-        //         }
-        //         if(sum == k)
-        //             count++;
-        //     }
-        // }
-
-        // return count;
-
-        // Optimal Approach
-
+        
         int n = nums.size();
 
-        unordered_map<int,int> mp;
-        mp.insert({0,1});
+        unordered_map<int,int> mp; // store {sum, freuency}
+        mp[0] = 1; // means sum=0 appeared once, before we start
 
-        int result = 0;
-        int cumSum = 0;
+        int sum = 0;
+        int count = 0;
 
-        for(int i=0;i<n;i++){
+        for(int i=0; i<n; i++){
             
-            cumSum += nums[i];
-            if(mp.find(cumSum-k) != mp.end())
-                result +=  mp[cumSum-k];
+            sum += nums[i];
 
-            mp[cumSum]++;
+            if(mp.find(sum-k) != mp.end()) // means, we have earlier seen the subarray sum in map
+                count += mp[sum-k];
+
+            mp[sum]++; // store the current sum frequency in map
         }
-        return result;
+        return count;
     }
 };
