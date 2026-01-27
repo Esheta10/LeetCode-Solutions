6c245f05b650a725a34ce598397648aa57af6d36
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-27 22:48:25 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.53%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,50 +1,52 @@
 class Solution {
 public:
-    int findLeftMost(vector<int>& nums,int target,int n)
-    {
-        int l=0,r=n-1;
-        int left_most = -1;
-        while(l<=r)
-        {
-            int mid = l + (r-l)/2;
-            if(nums[mid]==target)
-            {
-                left_most = mid;//possibly my answer
-                r = mid-1;//look more on left
-            }
-            else if(nums[mid] < target)
-                l = mid+1;
-            else
-                r = mid-1;
-        }
-        return left_most;
+    int findFirst(vector<int>& nums, int target, int n){
 
-    }
-    int findRightMost(vector<int>& nums,int target,int n)
-    {
-        int l=0,r=n-1;
-        int right_most = -1;
-        while(l<=r)
-        {
-            int mid = l+(r-l)/2;
-            if(nums[mid]==target)
-            {
-                right_most=mid;//posiibly my answer
-                l=mid+1;//look more on right
-            }
-            else if(nums[mid]<target)
-                l=mid+1;
-            else 
-                r=mid-1;
+        int leftMost = -1;
+
+        int left = 0;
+        int right = n-1;
+
+        while(left <=  right){
+
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] == target){
+                leftMost = mid;
+                right = mid-1; // search more towwards the left
+            } else if(nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
         }
-        return right_most;
+        return leftMost;
+    }
+    int findLast(vector<int>& nums, int target, int n){
+
+        int rightMost = -1;
+        int left = 0;
+        int right = n-1;
+
+        while(left <=  right){
+
+            int mid = left + (right - left)/2;
+
+            if(nums[mid] == target){
+                rightMost = mid;
+                left = mid + 1;// search more towards right
+            } else if( nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return rightMost;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         int n = nums.size();
 
-        int left_most = findLeftMost(nums,target,n);
-        int right_most = findRightMost(nums,target,n);
+        int first = findFirst(nums, target, n);
+        int last = findLast(nums, target, n);
 
-        return {left_most,right_most};
+        return {first, last};
     }
 };
