be4908330d6d2c04da49a3fcc54594db8e5ebf2b
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-29 00:05:35 +0530

    Time: 0 ms (100%), Space: 8.5 MB (84.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -4,22 +4,22 @@ public:
         if(x==0 || x==1)
             return x;
 
-        long long low=0, high=x,ans=0;
-        while(low<=high)
-        {
-            long long mid = low + (high-low)/2;
+        int l = 1;
+        int r = x;
+        int result=0;
 
-            if(mid*mid==x)
+        while(l<=r){
+            int mid = l+(r-l)/2;
+
+            if(mid == x/mid)
                 return mid;
-
-            else if(mid*mid <x)
-            {
-                ans=mid;
-                low=mid+1;
+            else if( mid < x/mid){
+                result = mid;
+                l=mid+1;
+            } else {
+                r = mid-1;
             }
-            else
-                high=mid-1;
         }
-        return ans;
+        return result;
     }
 };
