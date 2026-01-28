28bfd9811f3c623657f7ed556ef833dce5d20ee3
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-28 23:23:00 +0530

    Time: 0 ms (100%), Space: 26.1 MB (92.1%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        //Approach-1:XOR Operation:-(O(n))
-        int result=0; 
-        for(int i=0;i<nums.size();i++)
-        {
-            result ^= nums[i];
-        }
-        return result;
-        //Approach-2:Binary Search:-O(log(n))
+        
+        int n = nums.size();
+
         int l=0;
-        int h=nums.size()-1;
-        while(l<h)
-        {
-            int mid=l+(h-l)/2;
-            if(mid % 2 == 0)//mid is even
-            {
-                if(nums[mid]==nums[mid+1])
-                    l=mid+2;
-                    else
-                    h=mid;
-            }
-            else//mid is odd
-            {
-                if(nums[mid]==nums[mid-1])
-                    l=mid+1;
+        int r=n-1;
+
+        while(l<r){
+            int mid = l+(r-l)/2;
+
+            if(mid % 2 == 0){   // mid is even
+
+                if(nums[mid] == nums[mid+1])
+                    l = mid+1; // single element is on right side
+                else 
+                    r = mid;
+            } else {
+                // mid is odd
+                if(nums[mid] == nums[mid-1])
+                    l = mid+1; // single element is on right side
                 else
-                    h=mid;
+                    r = mid;
             }
+
         }
-        return nums[l];
+
+        return nums[r];
     }
 };
