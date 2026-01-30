f8b1023624d8bbce2092d2cb9254db66c7d074f8
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-30 11:57:38 +0530

    Time: 0 ms (100%), Space: 12.7 MB (22.2%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,19 +1,37 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int low=0,high=nums.size()-1;
-        while(low < high)
-        {
-            int mid = low+(high-low)/2;
-            if(nums[mid] < nums[mid+1])
-            {
-                low=mid+1;
-            }
+        
+        int n = nums.size();
+
+        // edge case: single element
+        if(n==1)
+            return 0;
+        
+        // check boundaries
+        if(nums[0] > nums[1])
+            return 0;
+        
+        if(nums[n-1] > nums[n-2])
+            return n-1;
+
+        // fix the binary search range
+        int left = 1;
+        int right = n-2;
+
+        while(left <= right){
+
+            int mid = left + (right-left)/2;
+
+            if(nums[mid] > nums[mid+1] && nums[mid] > nums[mid-1])
+                return mid;
+            // peak must be on right
+            else if(nums[mid] < nums[mid+1])
+                left = mid+1;
+            // peak must be on left
             else
-            {
-                high = mid;
-            }
+                right = mid-1;
         }
-        return low;
+        return -1;
     }
 };
