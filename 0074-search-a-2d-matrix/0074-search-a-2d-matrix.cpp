ea99ca1b1bf6289c87d8b967e8bb5c1ff5e0f40f
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-01-31 11:55:34 +0530

    Time: 0 ms (100%), Space: 13.4 MB (43.36%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,43 +1,26 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        //Approach-1 O(m*n)
-        /*
-        int m = matrix.size();
-        int n = matrix[0].size();
+        
+        int row = matrix.size();
+        int col = matrix[0].size();
 
-        int i=0,j=n-1;
-        while(i<m && j>=0)
-        {
-            if(matrix[i][j]>target)
-                j--;
-            else if(matrix[i][j]<target)
-                i++;
-            else
+        // convert 2D matrix to 1D sorted array
+        int left = 0;
+        int right = row*col - 1;
+
+        while(left <= right){
+            int mid = left + (right -left)/2;
+
+            //convert 1D array to 2D co-ordinates
+            int midValue = matrix[mid/col][mid%col];
+            if(midValue == target)
                 return true;
-        }
-        return false;   */
-        //Approach-2 (Using Binary Search) O(log(m*n))
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        int start = 0;
-        int end= m*n-1;
-
-        while(start<=end)
-        {
-            int mid = start+(end-start)/2;
-            int row=mid/n;
-            int col=mid%n;
-
-            if(matrix[row][col]<target)
-                start=mid+1;
-            else if(matrix[row][col]>target)
-                end=mid-1;
+            else if(midValue < target)
+                left = mid+1;
             else
-                return true;
+                right = mid-1;
         }
         return false;
-
     }
 };
