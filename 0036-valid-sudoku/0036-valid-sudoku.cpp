16fc89594f6d9fd9f1d95193cb1824d8f2b25fcc
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-02-11 13:38:32 +0530

    Time: 9 ms (16.86%), Space: 24.1 MB (34.77%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    bool validBoard(vector<vector<char>>& board, int sr, int er, int sc, int ec){
+
+        unordered_set<char> st;
+        for(int row=sr; row<=er; row++){
+            for(int col=sc; col<=ec; col++){
+
+                if(board[row][col] == '.') continue;
+                if(st.find(board[row][col]) != st.end())
+                    return false;
+                
+                st.insert(board[row][col]);
+            }
+        }
+        return true;
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        
+        // validate rows
+        for(int row=0; row<9; row++){
+            unordered_set<char> st;
+            for(int col=0; col<9; col++){
+                if(board[row][col]=='.') continue;
+
+                if(st.find(board[row][col]) != st.end())
+                    return false;
+                
+                st.insert(board[row][col]);
+            }
+        }
+
+        // validate cols
+        for(int col=0; col<9; col++){
+            unordered_set<char> st;
+            for(int row=0; row<9; row++){
+                if(board[row][col] == '.') continue;
+
+                if(st.find(board[row][col]) != st.end())
+                    return false;
+
+                st.insert(board[row][col]);
+            }
+        }
+
+        // validate 3x3 box
+        for(int sr=0; sr<9; sr+=3){
+            int er = sr+2;
+            for(int sc=0; sc<9; sc+=3){
+                int ec = sc+2;
+                if(!validBoard(board,sr, er, sc, ec))
+                    return false;
+            }
+        }
+        return true;
+    }
+};
