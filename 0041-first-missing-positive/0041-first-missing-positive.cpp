ad91824cfb8dee1a1440a945a2fac5566b7c999c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-02-12 20:06:57 +0530

    Time: 0 ms (100%), Space: 55 MB (85.11%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.cpp
+++ 0041-first-missing-positive/0041-first-missing-positive.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
+        
         int n = nums.size();
-        bool contains1 = false;
 
-        for (int i = 0; i < n; i++) {
-            // Check whether 1 is in the original array
-            if (nums[i] == 1) {
-                contains1 = true;
+        for(int i=0; i<n; i++){
+
+            while(true){
+
+                int current = nums[i];
+
+                if(current<1 || current>n)
+                    break;
+
+                int rightPlace = current-1;
+
+                if(nums[rightPlace] == current)
+                    break; // element apni sahi jagah par hai
+
+                swap(nums[rightPlace], nums[i]);
             }
-            if (nums[i] <= 0 || nums[i] > n) {
-                nums[i] = 1 ;
-            }      
         }
 
-        if (!contains1) return 1;
-
-        // Mark whether integers 1 to n are in nums
-        // Use index as a hash key and negative sign as a presence detector.
-        for (int i = 0; i < n; i++) {
-            
-            int val = abs(nums[i]);
-            int idx = val-1;
-            
-            if(nums[idx] < 0) continue;
-            nums[idx] *= -1;
-            
-        }
-
-        // First positive in nums is smallest missing positive integer
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0)
+        for(int i=0; i<n; i++){
+            if(nums[i] != i+1)
                 return i+1;
         }
-
-        return n + 1;
+        return n+1;
     }
 };
