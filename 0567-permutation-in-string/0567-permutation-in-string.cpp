b6aabf55371df450d1e562486509da32cd5dbd66
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-02-19 13:09:22 +0530

    Time: 0 ms (100%), Space: 9.8 MB (88.17%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -2,22 +2,41 @@ class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
         
-        int m = s1.length();
-        int n = s2.length();
+        int m = s1.size();
+        int n = s2.size();
 
         if(m > n)
             return false;
 
-        sort(s1.begin(), s1.end());
+       // frequency vectors for s1 and the current window in s2
+       vector<int> s1_freq(26, 0);
+       vector<int> s2_freq(26, 0);
 
-        for(int i=0; i<=n-m; i++){
+        // fill frequency of characters in s1
+       for(char &ch : s1){
+            s1_freq[ch-'a']++;
+       }
 
-            string substring = s2.substr(i, m);
-            sort(substring.begin(), substring.end());
+       // slide the window over s2
+       int i=0; // left index of the sliding window
+       int j=0; // right index of the sliding window
 
-            if(s1 == substring)
+       while(j<n){
+
+            s2_freq[s2[j] - 'a']++; // include a new character from the right end of the window
+
+            // check if the current window size matches the size of s1
+            if(j-i+1 > m){
+                s2_freq[s2[i] - 'a']--;
+                i++;
+            }
+
+            // check if current window's frequency matches s1's frequency
+            if(s1_freq == s2_freq)
                 return true;
-        }
+
+            j++;
+       }
         return false;
-    }
+    }   
 };
