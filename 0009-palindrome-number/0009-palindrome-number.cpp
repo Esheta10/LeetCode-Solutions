68d7c0855bff029f79d871553f0c59102c14a60e
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-02-25 15:36:20 +0530

    Time: 0 ms (100%), Space: 8.5 MB (65.5%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.cpp
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        int rev=0;
-        //Check for negative numbers and multiples of 10 (except 0)
-        if(x<0 || (x%10==0 && x!=0))
-            return false;
-        while(x>rev)
-        {
-            rev=rev*10+ x%10;
-            x=x/10;
+        
+       long long temp = x;
+       long long rev=0;
+
+        while(x>0){
+
+            long long lastDigit = x%10;
+            rev = rev*10 + lastDigit;
+            x = x/10;
         }
-       return x==rev || x==rev/10;
+        return rev == temp;
     }
 };
