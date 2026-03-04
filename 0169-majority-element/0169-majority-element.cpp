59356b507d516e393d78df918a495e6dd58bba52
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-03-04 19:55:02 +0530

    Time: 3 ms (29.64%), Space: 28.1 MB (62.95%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -6,14 +6,15 @@ public:
         int count = 0;
         int maj = NULL;
 
-        for(int i=0;i<n;i++){
-            if(count == 0){
+        for(int i=0; i<n; i++){
+
+            if(nums[i] == maj){
+               count++;
+            } else if(count == 0){
                 maj = nums[i];
                 count = 1;
-            } else if( maj == nums[i]){
-                count++;
-            } else {
-                count --;
+            } else{
+                count--;
             }
         }
         return maj;
