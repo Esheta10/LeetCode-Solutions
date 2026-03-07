e08f546660c20208f27c1c0972173813182336e0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-03-07 18:31:18 +0530

    Time: 0 ms (100%), Space: 14.9 MB (35.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,17 +3,17 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
         int n = nums.size();
+        unordered_map<int, int> mp;
 
-        unordered_map<int,int> mp;
-
-        for(int i=0; i<n;i++){
+        for(int i=0; i<n; i++){
 
             int remaining = target - nums[i];
             if(mp.find(remaining) != mp.end())
-                return {mp[remaining], i};  // store the remaining element's index, already present in map , current index
+                return {mp[remaining], i};
 
-            mp[nums[i]] = i;// if not found, store the current element and its index
+            mp[nums[i]] = i;
         }
+
         return {-1,-1};
     }
 };
