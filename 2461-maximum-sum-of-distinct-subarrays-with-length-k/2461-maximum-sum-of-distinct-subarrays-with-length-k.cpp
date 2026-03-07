e8a245d372b54ad183cb408248b83335307cf628
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-03-07 13:00:39 +0530

    Time: 119 ms (27.6%), Space: 99.6 MB (33.44%) - LeetHub

---
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    long long maximumSubarraySum(vector<int>& nums, int k) {
+        
+        int n = nums.size();
+        unordered_set<int> st;
+
+        long long windowSum = 0 , maxSum = 0;
+        int left = 0;
+
+        for(int right=0; right<n; right++){
+
+            // if duplicates found, shrink from left until its gone
+            while(st.find(nums[right]) != st.end()){
+
+                st.erase(nums[left]);
+                windowSum -= nums[left];
+                left++;
+            }
+            
+            // add new element
+            st.insert(nums[right]);
+            windowSum += nums[right];
+
+            // check if window size is exactly k
+            if(right-left+1 == k){
+
+                maxSum = max(maxSum, windowSum);
+                // shrink window by 1 from left for next iteration
+                st.erase(nums[left]);
+                windowSum -= nums[left];
+                left++;
+            }
+        }
+        return maxSum;
+    }
+};
