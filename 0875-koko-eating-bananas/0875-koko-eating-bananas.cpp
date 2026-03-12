efda25116886c4dfb8ef1783c83667e7d569703c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-03-12 16:35:30 +0530

    Time: 3 ms (94.97%), Space: 23 MB (12.31%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
+    // check if koko can finish all banana piles in h hours at speed mid
     bool canEatAllBananas(vector<int>& piles, int h, int mid){
 
         int actualHours = 0;
 
         for(int &pile : piles){
 
-            actualHours += pile / mid; // hours
+            actualHours += pile / mid; // full hours for this pile 
 
             if(pile % mid != 0)
-                actualHours++;
+                actualHours++; // leftover bananas ? +1 hour
         }
-        return actualHours <= h;
+        return actualHours <= h; // can she finish in time ?
     }
     int minEatingSpeed(vector<int>& piles, int h) {
         
-        int left = 1;
-        int right = *max_element(piles.begin(), piles.end());
+        int left = 1; // min speed
+        int right = *max_element(piles.begin(), piles.end()); // max speed in worst case scenario
 
         while(left < right){
 
-            int mid = left + (right - left)/2;
+            int mid = left + (right - left)/2; // candidate speed
 
             if(canEatAllBananas(piles, h, mid))
-                right = mid;
+                right = mid; // speed works, try smaller
             else
-                left = mid + 1;
+                left = mid + 1; // too slow, need faster
         }
-        return left;
+        return left; // minimum valid speed
     }
 };
