0cbd84a88fe077ac265429d2749be4e3a7391870
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-12 15:41:42 +0530

    Time: 0 ms (100%), Space: 8.9 MB (67.02%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<char> stk;
-        for(char ch:s)
-        {
-           if (ch == '(' || ch == '{' || ch == '[') {
-            // Push opening brackets onto the stack
-            stk.push(ch);
-        } else {
-            // If the stack is empty, we have a closing bracket without an opening
-            if (stk.empty()) return false;
-            
-            // Check if the closing bracket matches the opening bracket on top of the stack
-            if ((ch == ')' && stk.top() == '(') ||
-                (ch == '}' && stk.top() == '{') ||
-                (ch == ']' && stk.top() == '[')) {
-                stk.pop(); // Pop the matched opening bracket
-            } else {
-                // Mismatched closing bracket
-                return false;
-            }
+        
+        stack<char> st;
 
-         }
-     }
-      // If the stack is empty, all brackets were matched
-    return stk.empty();
+        for(char &ch: s){
+
+            if(ch == '(' || ch == '{' || ch == '[')
+                st.push(ch);
+            else {
+                if(st.empty())
+                    return false; // nothing opened -> mismatch
+                
+                char top = st.top();
+                st.pop();
+
+                if(ch == ')' && top != '(')
+                    return false;
+                if(ch == '}' && top != '{')
+                    return false;
+                if(ch == ']' && top != '[')
+                    return false;
+            }
+        }
+        return st.empty(); // any unclosed openers left -> return false
     }
 };
