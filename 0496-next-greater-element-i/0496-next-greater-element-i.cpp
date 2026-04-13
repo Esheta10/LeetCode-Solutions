72212ecd2cddf65756aef43941bcc8cc23810316
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-13 15:59:14 +0530

    Time: 7 ms (10.64%), Space: 12.3 MB (91.91%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        int m = nums1.size();
-        int n = nums2.size();
-        vector<int> result;
+        vector<int> ans;
 
-        for(int i=0;i<m;++i)
-        {
-            int foundIndex=-1;
-            for(int j=0;j<n;++j)
-            {
-                // Step 1: Find the index of nums1[i] in nums2
-                if(nums2[j] == nums1[i])
-                {
-                    foundIndex = j;
-                    break;
-                }        
-            }
-            // Step 2: Find the next greater element after foundIndex
-            int nextGreater=-1;
-            for(int k = foundIndex + 1; k<n; ++k)
-            {
-                if(nums2[k] > nums1[i])
-                {
+        for(int i=0; i < nums1.size(); i++){
+
+            // step-1 : nums2[j] mein nums1[i] dhundho
+            int j = 0;
+            while(nums2[j] != nums1[i])
+                j++;
+
+            // step-2 : j ke right mein pehla bada element dhundho
+            int nextGreater = -1;
+            for(int k = j+1; k<nums2.size(); k++){
+
+                if(nums2[k] > nums1[i]){
                     nextGreater = nums2[k];
                     break;
                 }
             }
-            result.push_back(nextGreater);
-        }   
-        return result;
+            ans.push_back(nextGreater);
+        }
+        return ans;
     }
 };
