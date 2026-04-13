6bdfeaee6c2299ca177baa2eed1d10ddb1191779
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-13 14:46:11 +0530

    Time: 7 ms (19.81%), Space: 23.5 MB (43.23%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -0,0 +1,72 @@
+class MyCircularQueue {
+private:
+    vector<int> arr;
+    int front, rear, count, capacity;
+public:
+    MyCircularQueue(int k) {
+        
+        capacity = k;
+        arr.resize(k);
+        front = -1;
+        rear = -1;
+        count = 0;
+    }
+    
+    bool enQueue(int value) {
+        
+        if(isFull())
+            return false;
+        
+        if(isEmpty())
+            front = rear = 0;
+        else 
+            rear = (rear + 1) % capacity;
+
+        arr[rear] = value;
+        count++;
+        return true;
+    }
+    
+    bool deQueue() {
+        
+        if(isEmpty())
+            return false;
+
+        if(front == rear)
+            front = rear = -1;
+        else
+            front = (front + 1) % capacity;
+
+        count--;
+        return true;
+    }
+    
+    int Front() {
+        
+        return isEmpty() ? -1 : arr[front];
+    }
+    
+    int Rear() {
+
+        return isEmpty() ? -1 : arr[rear]; 
+    }
+    
+    bool isEmpty() {
+        return count == 0;
+    }
+    
+    bool isFull() {
+        return count == capacity;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
