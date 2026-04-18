708cfb3ed31e44ee50cdfdc949a46f0e64589d05
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-18 10:58:08 +0530

    Time: 0 ms (100%), Space: 43.6 MB (48.57%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -3,26 +3,25 @@ public:
     int maxSubarraySumCircular(vector<int>& nums) {
         
         int n = nums.size();
-        
+        int max_sum = nums[0], curr_max_sum = nums[0];
+        int min_sum = nums[0], curr_min_sum = nums[0];
         int totalSum = 0;
-        int maxSum = nums[0], curr_max_sum = nums[0];
-        int minSum = nums[0], curr_min_sum = nums[0];
 
         for(int i=1; i<n; i++){
 
             totalSum += nums[i];
-
             curr_max_sum = max(curr_max_sum + nums[i], nums[i]);
-            maxSum = max(maxSum, curr_max_sum);
+            max_sum = max(max_sum, curr_max_sum);
 
             curr_min_sum = min(curr_min_sum + nums[i], nums[i]);
-            minSum = min(minSum, curr_min_sum);
+            min_sum = min(min_sum, curr_min_sum);
         }
         totalSum += nums[0];
 
-        if(maxSum < 0)
-            return maxSum; // all negatives, return the least negative
+        if(max_sum <0)
+            return max_sum;
 
-        return max(maxSum, totalSum - minSum);
+        return max(max_sum, totalSum - min_sum);
+
     }
 };
