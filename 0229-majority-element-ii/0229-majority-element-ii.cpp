e2c2a549b3b3a9b7708508a82d33a5b3f576ee1a
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-18 11:40:58 +0530

    Time: 0 ms (100%), Space: 23.5 MB (82.85%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -3,25 +3,25 @@ public:
     vector<int> majorityElement(vector<int>& nums) {
         
         int n = nums.size();
-        
-        int count1 = 0;
+
         int maj1 = NULL;
+        int count1 = 0;
 
-        int count2 = 0;
         int maj2 = NULL;
+        int count2 = 0;
 
-        // Phase-1: Find candidate
-        for(int i=0; i<n; i++){
 
-            if(nums[i] == maj1){
+        for(int num : nums){
+
+            if(num == maj1)
                 count1++;
-            } else if(nums[i] == maj2){
+            else if(num == maj2)
                 count2++;
-            } else if(count1 == 0){
-                maj1 = nums[i];
+            else if(count1 == 0){
+                maj1 = num;
                 count1 = 1;
             } else if(count2 == 0){
-                maj2 = nums[i];
+                maj2 = num;
                 count2 = 1;
             } else {
                 count1--;
@@ -29,21 +29,24 @@ public:
             }
         }
 
-        //Phase-2: Verify candidate
+
+        //Verifying the majority element 
         int freq1 = 0, freq2 = 0;
-        vector<int> result;
         for(int num: nums){
             if(num == maj1)
                 freq1++;
             else if(num == maj2)
                 freq2++;
         }
-
+        
+        vector<int> result;
         if(freq1 > floor(n/3))
             result.push_back(maj1);
         if(freq2 > floor(n/3))
             result.push_back(maj2);
 
+
         return result;
+
     }
 };
