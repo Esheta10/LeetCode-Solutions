921e54687066b2c38a5789ac14fa41201662da68
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-18 07:03:44 +0530

    Time: 0 ms (100%), Space: 24.2 MB (7.34%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,17 +1,28 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-      int n=nums.size();
-       int insertPos=0;
-       for(int j=0;j<n;j++)
-       {
-            if(nums[j]!=0)
-            {
-                nums[insertPos]=nums[j];
-                insertPos++;
+        
+        int n = nums.size();
+
+        vector<int> temp(n);
+        int j = 0;
+    
+        for(int i=0; i<n; i++){
+
+            if(nums[i] != 0){
+                temp[j] = nums[i];
+                j++;
             }
-       }
-       for(int i=insertPos;i<n;i++)
-            nums[i]=0;    
+        }
+        // now fill the remaining array with 0's
+        while(j<n){
+            temp[j] = 0;
+            j++;
+        }
+
+        // now copy all the elements from temp to original array
+        for(int i=0; i<n; i++){
+            nums[i] = temp[i];
+        } 
     }
 };
