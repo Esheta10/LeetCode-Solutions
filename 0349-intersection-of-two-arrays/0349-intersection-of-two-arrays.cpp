02bf24fda01ae0bd3469f9e15d0e2e0395b150b0
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-19 15:30:36 +0530

    Time: 0 ms (100%), Space: 13.9 MB (81.25%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        //APPROACH-1
-        /* unordered_set<int> st1(nums1.begin(),nums1.end());
-        unordered_set<int> st2;
         
-        for(int &num: nums2)
-        {
-            if(st1.find(num)!=st2.end())
-                st2.insert(num);
-        }
         vector<int> result;
-        for(auto &it:st2)
-        {
-            result.push_back(it);
-        }
-        return result;  */
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
+
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
         int m = nums1.size();
         int n = nums2.size();
 
-        int i=0;//nums1
-        int j=0;//nums2
+        int i=0,j=0;
 
-        vector<int> result;
-        while(i<m && j<n)
-        {
-            if(nums1[i]==nums2[j])
-            {
-                  result.push_back(nums1[i]);//nums2[j]
+        while(i<m && j<n){
 
-                  while(i<m-1 && nums1[i]==nums1[i+1])
-                    i++;
-                  while(j<n-1 && nums2[j]==nums2[j+1])
-                    j++;
+            if(nums1[i] == nums2[j]){
+                result.push_back(nums1[i]);
 
+                // Check for duplicates
+                while(i<m-1 && nums1[i] == nums1[i+1]) i++;
+                
+                while(j<n-1 && nums2[j] == nums2[j+1]) j++;
+               
                 i++;
                 j++;
-            }
-            else if(nums1[i]<nums2[j])
+            } else if(nums1[i] < nums2[j])
                 i++;
             else
                 j++;
