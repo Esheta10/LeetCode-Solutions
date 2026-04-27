4bfb5c3d6717a71caa9c2f67ddc2fa5eef603571
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-04-27 16:44:21 +0530

    Time: 0 ms (100%), Space: 9.2 MB (71.21%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int m,n;
+    int result;
+    int nonObstacles;
+    void backtrack(vector<vector<int>>& grid, int i, int j, int count){
+
+        if(i<0 || j<0 || i>=m || j>=n || grid[i][j] == -1)
+            return;
+
+        if(grid[i][j] == 2){
+
+            if(count == nonObstacles)
+                result++;
+
+            return;
+        }  
+
+        grid[i][j] = -1;
+
+        if(i-1>=0 && grid[i-1][j] != -1) backtrack(grid, i-1, j, count+1);
+        if(i+1<m && grid[i+1][j] != -1) backtrack(grid, i+1, j, count+1);
+        if(j-1>=0 && grid[i][j-1] != -1) backtrack(grid, i, j-1, count+1);
+        if(j+1<n && grid[i][j+1] != -1) backtrack(grid, i, j+1, count+1);
+
+        grid[i][j] = 0;
+
+    }
+    int uniquePathsIII(vector<vector<int>>& grid) {
+        
+        m = grid.size();
+        n = grid[0].size();
+
+        result = 0;
+        nonObstacles = 0;
+
+        int start_x = 0, start_y = 0;
+
+        for(int i=0; i<m; i++){
+
+            for(int j=0; j<n; j++){
+
+                if(grid[i][j] == 0)
+                    nonObstacles++; // count the no. of non-obstacles
+
+                if(grid[i][j] == 1){ // starting point
+                    start_x = i;
+                    start_y = j;
+                }
+            }
+        }
+
+        nonObstacles += 1; // sinces we have both 0's and the initial starting point - 1 as non-obstacles
+
+        int count = 0;
+
+        backtrack(grid, start_x, start_y, count);
+
+        return result;
+    }
+};
