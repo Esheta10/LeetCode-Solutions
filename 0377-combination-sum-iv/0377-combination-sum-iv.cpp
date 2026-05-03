fed534695a819ff599f26c5a5761ac51832692f4
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-05-03 15:48:30 +0530

    Time: 0 ms (100%), Space: 9.9 MB (7.4%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.cpp
+++ 0377-combination-sum-iv/0377-combination-sum-iv.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
     int n;
-    int t[1001][201];
-    int solve(int idx, vector<int>& nums, int target){
+    int t[201][1001];
+    int backtrack(int idx, vector<int>& nums, int target){
 
         if(target == 0)
             return 1;
-        
-        if(idx >= n || target < 0)
+
+        if(idx >= n || target <0)
             return 0;
 
-        if(t[target][idx] != -1)
-            return t[target][idx];
-            
-        int take = solve(0, nums, target-nums[idx]);
-        int skip = solve(idx+1, nums,target);
+        if(t[idx][target] != -1)
+            return t[idx][target];
 
-        return  t[target][idx] = take + skip;
+        int take_idx = backtrack(0, nums, target - nums[idx]);
+        int reject_idx = backtrack(idx+1, nums, target);
+
+        return t[idx][target] = take_idx + reject_idx;
     }
     int combinationSum4(vector<int>& nums, int target) {
         
         n = nums.size();
         memset(t, -1, sizeof(t));
-        return solve(0, nums, target);
-
+        return backtrack(0, nums, target);
     }
 };
