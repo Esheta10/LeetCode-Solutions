341218066ba4b2b9fea96cb4c6a4b2180c2dc70f
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-05-21 23:41:24 +0530

    Time: 0 ms (100%), Space: 13.6 MB (41.59%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.cpp
+++ 0035-search-insert-position/0035-search-insert-position.cpp
@@ -5,16 +5,18 @@ public:
         int n = nums.size();
         int left = 0;
         int right = n-1;
+
         int ans = n;
+
         while(left <= right){
 
             int mid = left + (right - left)/2;
 
             if(nums[mid] >= target){
-                ans = mid;
-                right = mid - 1;
+                ans = mid;  // potential answer
+                right = mid-1; // still serach left to find an even smaller value where nums[mid] >= x
             } else {
-                left = mid + 1;
+                left = mid+1;
             }
         }
         return ans;
