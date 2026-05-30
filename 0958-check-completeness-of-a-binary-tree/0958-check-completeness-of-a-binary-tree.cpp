e48b1afbc24452455ba971b33ed01df91a76f4ab
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-05-30 23:47:09 +0530

    Time: 0 ms (100%), Space: 13.8 MB (58.17%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -11,54 +11,31 @@
  */
 class Solution {
 public:
-    int countNodes(TreeNode* root){
-
-        if(root == NULL)
-            return 0;
-        
-        return 1 + countNodes(root->left) + countNodes(root->right);
-    }
-    bool dfs(TreeNode* root, int idx, int totalNodes){
-
-        if(root == NULL)
-            return true;
-
-        if(idx > totalNodes)
-            return false;
-
-        return dfs(root->left, 2*idx,totalNodes) && dfs(root->right, 2*idx+1,totalNodes);
-    }
     bool isCompleteTree(TreeNode* root) {
         
-        // Using Level-order Traversal (BFS)
-        // queue<TreeNode*> q;
-        // q.push(root);
+        // using BFS
 
-        // bool past = false; // kya aapne pehle kiss NULL node ko dekha hai?
-        // while(q.empty() == false){
+        queue<TreeNode*> que;
 
-        //     TreeNode* node = q.front();
-        //     q.pop();
+        que.push(root);
 
-        //     if(node == NULL){
-        //         past = true;
-        //     } else {
-        //         if(past == true)
-        //             return false;
+        bool past = false;
 
-        //         q.push(node->left);
-        //         q.push(node->right);
-        //     }
-           
-        // }
-        // return true;
+        while(!que.empty()){
 
-        // Using DFS
+            TreeNode* node = que.front();
+            que.pop();
 
-        int totalNodes = countNodes(root);
-
-        int i = 1;
-
-        return dfs(root, i, totalNodes);
+            if(node == NULL){
+                past = true;
+            } else{
+                if(past == true){
+                    return false;
+                }
+                que.push(node->left);
+                que.push(node->right);
+            }
+        }
+        return true;
     }
 };
