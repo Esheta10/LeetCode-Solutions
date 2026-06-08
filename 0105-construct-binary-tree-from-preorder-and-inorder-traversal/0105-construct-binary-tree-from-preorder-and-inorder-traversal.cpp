9a12806fdde95de9d59f2f6584918baad2f8e664
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-06-08 16:17:18 +0530

    Time: 7 ms (35.51%), Space: 27.2 MB (82.57%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -11,26 +11,27 @@
  */
 class Solution {
 public:
-    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end,int &idx){
-        
+    TreeNode* solve(vector<int>& preorder, vector<int>& inorder, int start, int end, int &idx){
+
         if(start > end)
             return NULL;
 
         int rootVal = preorder[idx];
-        int i = start;
-
-        for(; i<=end; i++){
+        
+        int i=start;
+        for(; i<end; i++){
 
             if(inorder[i] == rootVal)
                 break;
         }
-        idx++;
 
+        idx++;
         TreeNode* root = new TreeNode(rootVal);
-        root->left = solve(preorder,inorder,start,i-1,idx);
-        root->right = solve(preorder,inorder,i+1,end,idx);
+        root->left = solve(preorder, inorder, start, i-1, idx);
+        root->right = solve(preorder, inorder, i+1, end, idx);
 
         return root;
+
     }
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         
@@ -38,6 +39,6 @@ public:
 
         int idx = 0;
 
-        return solve(preorder,inorder,0,n-1,idx);
+        return solve(preorder, inorder, 0, n-1, idx);
     }
 };
