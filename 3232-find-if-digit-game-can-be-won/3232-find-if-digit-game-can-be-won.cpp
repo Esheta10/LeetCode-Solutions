8960a9e72f5a1c000a28dba0e11d036cce1dd9ab
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-08-01 00:10:57 +0530

    Time: 0 ms (100%), Space: 28.7 MB (11.24%) - LeetHub

---
+++ 3232-find-if-digit-game-can-be-won/3232-find-if-digit-game-can-be-won.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool canAliceWin(vector<int>& nums) {
+        
+        int single_sum=  0;
+        int double_sum = 0;
+
+        int n = nums.size();
+
+        for(int i=0; i<n; i++){
+
+            if(nums[i] <= 9)
+                single_sum += nums[i];
+            else
+                double_sum += nums[i];
+        }
+
+        if(single_sum < double_sum || single_sum > double_sum)
+            return true;
+        else
+            return false;
+
+    }
+};
