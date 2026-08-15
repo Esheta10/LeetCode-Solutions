9aed2ffa62e1801a9ed683dee1302f5541e8d08c
Esheta Jaiswal <109418467+Esheta10@users.noreply.github.com>
2026-08-15 10:13:21 +0530

    Time: 0 ms (100%), Space: 11.9 MB (17.44%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -2,36 +2,48 @@ class Solution {
 public:
     string removeKdigits(string num, int k) {
         
-        vector<char> st;
+        stack<char> st;
 
-        for(char digit : num){
+        for(char &ch: num){
 
-            while(!st.empty() && k>0 && st.back() > digit){
-                st.pop_back(); // remove larger element
+            while(!st.empty() && k>0 && st.top() > ch){
+                st.pop();
                 k--;
             }
-
-            st.push_back(digit);
+            
+            st.push(ch);
         }
 
-        // trim remaining k from end --> agar number ascending ordr mein ho and k exist karta ho
-        while(k> 0){
-            st.pop_back();
+
+        // agar num string already ascending order mein ho like - 12345, then remove last k digits directly
+        while(k>0){
+            st.pop();
             k--;
         }
 
-        // build result, skip leading zeros
+
+        // for leading zeros -> 10200, k=1
+        // output is -> 0200 but we must ignore leading 0 and only return 200 as output
+        string numArray = "";
+        // stack se elements nikal kar string mein daalna
+        while(!st.empty()){
+            numArray += st.top();
+            st.pop();
+        }
+        // qki stack se lements ulte order mein extract hoga
+        reverse(numArray.begin(), numArray.end());
+
         string result = "";
         bool leadingZeros = true;
 
-        for(char c: st)
-        {
+        for(char &c : numArray){
+
             if(leadingZeros && c == '0')
-                continue;
+                continue; // skip this iteration
 
             leadingZeros = false;
             result += c;
         }
-        return result.empty() ? "0" : result;
+        return result.empty() ? "0" : result; // "22" , k=2 mein output 0 hoga 
     }
 };
